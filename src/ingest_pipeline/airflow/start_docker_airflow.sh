125f797f6de6e8b49589624c38f75cf202e8af47
Joel Welling <welling@psc.edu>
2019-10-22 23:11:36 -0400

    Script to start up the docker airflow instance

---
+++ src/ingest_pipeline/airflow/start_docker_airflow.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+run_id=`docker run -d -p 8080:8080 \
+	-v $PWD/dags:/usr/local/airflow/dags \
+	-v $PWD/plugins/:/usr/local/airflow/plugins \
+	puckel/docker-airflow webserver | cut -c1-12`
+run_name=`docker ps --format "{{.ID}} {{.Names}}" | grep ${run_id} | awk '{print $2}'`
+echo $run_name
