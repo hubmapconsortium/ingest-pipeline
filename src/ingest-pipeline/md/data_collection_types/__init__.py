55425613af79c4ceac783d65e7db01b11446e3e8
Joel Welling <welling@psc.edu>
2020-04-03 18:00:46 -0400

    Add devtest datacollectiontype

--- src/ingest-pipeline/md/data_collection_types/__init__.py
+++ src/ingest-pipeline/md/data_collection_types/__init__.py
@@ -2,6 +2,7 @@ from .ims_data_collection import IMSDataCollection
 from .rnaseq_10x_data_collection import RNASEQ10XDataCollection
 from .stanford_codex_data_collection import StanfordCODEXDataCollection
 from .akoya_codex_data_collection import AkoyaCODEXDataCollection
+from .devtest_data_collection import DEVTESTDataCollection
 
 __all__ = [IMSDataCollection, RNASEQ10XDataCollection, StanfordCODEXDataCollection,
-           AkoyaCODEXDataCollection]
+           AkoyaCODEXDataCollection, DEVTESTDataCollection]
