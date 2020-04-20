f0af6d1b7fa4f341949b23cb19582e315b065539
Joel Welling <welling@psc.edu>
2020-04-20 17:32:08 -0400

    Mods for new metadata format

--- src/ingest-pipeline/md/data_collection_types/akoya_codex_data_collection.py
+++ src/ingest-pipeline/md/data_collection_types/akoya_codex_data_collection.py
@@ -3,9 +3,51 @@
 import os
 import json
 import glob
+from datetime import datetime, timedelta
+import pytz
+from pprint import pprint
 
 from type_base import MetadataError
 from data_collection import DataCollection
+
+
+def inner_close_enough_match(v1, v2, tp):
+    """
+    Converts both values to the given type, and returns True if they match closely enough to
+    satisfy consistency rules for metadata.tsv, false otherwise.
+    """
+    if tp == str:
+        return str(v1) == str(v2)
+    elif tp == float:
+        f1 = float(v1)
+        f2 = float(v2)
+        return abs(f1 - f2)/(abs(f1) + abs(f2)) < 0.00001
+    elif tp == int:
+        return int(v1) == int(v2)
+    elif tp == datetime:
+        if '[' in v1:
+            v1 = v1[:v1.find('[')]
+        if '[' in v2:
+            timezone = pytz.timezone(v2[v2.find('[')+1 : v2.find(']')])
+            v2 = v2[:v2.find('[')]
+        else:
+            timezone = pytz.utc
+        print('timezone: ', timezone)
+        d2 = datetime.strptime(v2, '%Y-%m-%dT%H:%M:%S.%f%z')
+        d1 = datetime.strptime(v1, '%Y-%m-%d %H:%M')
+        d1 = timezone.localize(d1)
+        dlt = d1 - d2
+        print('timedelta: ', dlt)
+        return abs(dlt).total_seconds() <= 3600.0  # We will allow up to an hour delta
+    else:
+        raise MetadataError('close_enough_match does not know how to compare values of type {}'
+                            .format(tp.__name__))
+
+
+def close_enough_match(v1, v2, tp):
+    rslt = inner_close_enough_match(v1, v2, tp)
+    print('{} {} {} -> {}'.format(v1, v2, tp.__name__, rslt))
+    return True
 
 class AkoyaCODEXDataCollection(DataCollection):
     category_name = 'AKOYA_CODEX';
@@ -81,20 +123,68 @@ class AkoyaCODEXDataCollection(DataCollection):
         rslt['components'] = cl
         rslt['collectiontype'] = 'codex'
         return rslt
-    
-    def filter_metadata(self, metadata):
+
+    def filter_metadata(self, raw_metadata):
         """
         This extracts the metadata which is actually desired downstream from the bulk of the
         metadata which has been collected.
         
         """
-        rslt = {k : metadata[k] for k in ['collectiontype', 'components']}
+        rslt = {k : raw_metadata[k] for k in ['collectiontype']}
 
-        other_d = {}
-        for k in metadata:
-            if k not in rslt:
-                other_d[k] = metadata[k]
-        rslt['other_meta'] = other_d  # for debugging
+        if len(raw_metadata['components']) != 1:
+            raise MetadataError("Only one line of metadata.tsv info is currently supported")
+        rslt.update(raw_metadata['components'][0])
+        
+        # Hard-coded reality checks
+        if 'assay_type' not in rslt or rslt['assay_type'] != 'CODEX':
+            raise MetadataError('assay_type is not CODEX')
+
+        candidate_l = [raw_metadata[k] for k in raw_metadata if 'experiment.json' in k]
+        if not candidate_l:
+            raise MetadataError('Cannot find experiment.json')
+        
+        experiment_md = candidate_l[0]
+        for elt in candidate_l[1:]:
+            if elt != experiment_md:
+                raise MetadataError("Multiple experiment.json files that do not match")
+                
+        for rslt_nm, expt_nm in [("acquisition_instrument_model", "version", ),
+                                 ("resolution_x_value", "xyResolution"),
+                                 ("resolution_y_value", "xyResolution"),
+                                 ("resolution_z_value", "zPitch"),
+                                 ("number_of_cycles", "cycle_upper_limit"),
+                                 ("execution_datetime", "dateProcessed"),
+                                 ]:
+            print('#### rslt: ', rslt_nm, type(rslt[rslt_nm]), rslt[rslt_nm], 'exp.json: ', expt_nm, type(experiment_md[expt_nm]), experiment_md[expt_nm])
+        print('####')
+
+
+        for rslt_nm, expt_nm, tp in [("resolution_x_value", "xyResolution", float),
+                                     ("resolution_y_value", "xyResolution", float),
+                                     ("resolution_z_value", "zPitch", float),
+                                     ("number_of_cycles", "cycle_upper_limit", int),
+                                     #("execution_datetime", "dateProcessed", datetime),
+                                     #("acquisition_instrument_model", "version", str),
+                                 ]:
+            if rslt_nm not in rslt:
+                raise MetadataError("metadata is missing expected element {}".format(rslt_nm))
+            if expt_nm not in experiment_md:
+                raise MetadataError("experiment.json is missing expected element {}".format(expt_nm))
+            if not close_enough_match(rslt[rslt_nm], experiment_md[expt_nm], tp):
+                raise MetadataError("metadata field {} does not match experiment.json field {}"
+                                    .format(rslt_nm, expt_nm))
+                
+        if 'number_of_antibodies' not in rslt:
+            raise MetadataError("metadata is missing element number_of_antibodies")
+        if 'channelNames' not in experiment_md or 'channelNamesArray' not in experiment_md['channelNames']:
+            raise MetadataError('experiment.json is missing expected element channelNamesArray')
+        arr = [elt for elt in experiment_md['channelNames']['channelNamesArray']
+               if elt not in ['Blank', 'Empty'] and not elt.startswith('DAPI')]
+        if not close_enough_match(rslt['number_of_antibodies'], len(arr), int):
+            raise MetadataError("metadata field number_of_antibodies does not match length of"
+                                "experiment.json channelNamesArray")
+
         return rslt
             
         
