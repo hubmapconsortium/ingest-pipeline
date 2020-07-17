221113c6c0e8ff1c478811ee76ff90137910f148
Joel Welling <welling@psc.edu>
2020-07-16 20:01:53 -0400

    Working version of survey.py

--- src/ingest-pipeline/misc/tools/survey.py
+++ src/ingest-pipeline/misc/tools/survey.py
@@ -39,30 +39,24 @@ def _get_entity_prov(uuid, auth_tok):
 
 
 class Entity(object):
-    def __init__(self, uuid, auth_tok=None):
-        assert auth_tok, 'auth_tok is required'
-        data = {'query': {'ids': {'values': [f'{uuid}']}}}
-        r = requests.post(f'{SEARCH_URL}/portal/search',
-                          data=json.dumps(data),
-                          headers={'Authorization': f'Bearer {auth_tok}',
-                                   'Content-Type': 'application/json'})
-        #print(f'query was {r.request.body}')
-        if r.status_code >= 300:
-            r.raise_for_status()
-        jsn = r.json()
-        assert len(jsn['hits']['hits']) == 1, f'More than one hit on uuid {uuid}'
-        hit = jsn['hits']['hits'][0]
-        assert hit['_id'] == uuid, f"uuid {uuid} gave back uuid {hit['_id']}"
-        prop_dct = hit['_source']
-        assert prop_dct['uuid'] == uuid, f"uuid {uuid} gave back inner uuid {prop_dct['uuid']}"
-        self.uuid = uuid
-        self.prop_dct = prop_dct        
+    def __init__(self, prop_dct, entity_factory):
+        self.uuid = prop_dct['uuid']
+        self.prop_dct = prop_dct
+        self.entity_factory = entity_factory
+        self.display_doi = prop_dct['display_doi']
+
+    def describe(self, prefix='', file=sys.stdout):
+        print(f"{prefix}{self.uuid}: "
+              f"{self.display_doi} ",
+              file=file)
+        if self.kid_dataset_uuids:
+            for kid in self.kid_dataset_uuids:
+                self.kids[kid].describe(prefix=prefix+'    ', file=file)
 
 
 class Dataset(Entity):
-    def __init__(self, uuid, auth_tok=None):
-        super().__init__(uuid, auth_tok)
-        prop_dct = self.prop_dct
+    def __init__(self, prop_dct, entity_factory):
+        super().__init__(prop_dct, entity_factory)
         assert prop_dct['entity_type'] == 'Dataset', f"uuid {uuid} is a {prop_dct['entity_type']}"
         self.status = prop_dct['status']
         if 'metadata' in prop_dct:
@@ -80,40 +74,33 @@ class Dataset(Entity):
         self.kid_dataset_uuids = [elt['uuid'] for elt in prop_dct['immediate_descendants']
                                   if elt['entity_type'] == 'Dataset']
         self.data_types = prop_dct['data_types']
-        self.display_doi = prop_dct['display_doi']
         self.donor_uuid = prop_dct['donor']['uuid']
 
-        self.kid_datasets = None
-        self.parent_datasets = None
-
-
-    def init_relatives(self, auth_tok=None):
-        assert auth_tok, 'auth_tok is requried'
-        self.kid_datasets = [Dataset(kid_uuid, auth_tok=auth_tok)
-                             for kid_uuid in self.kid_dataset_uuids]
-        self.parent_entities = [Entity(parent_uuid, auth_tok=auth_tok)
-                                for parent_uuid in self.parent_uuids]
-
-        op_l = [uuid for uuid in self.parent_uuids if uuid not in self.parent_dataset_uuids]
-        print('Other parents: ', op_l)
-        if op_l:
-            parent_l = [Entity(uuid, auth_tok) for uuid in op_l]
-            print('ENTITY TYPE: ', parent_l[0].prop_dct['entity_type'])
-            pprint(parent_l[0].prop_dct.keys())
-        #parent = Dataset(op_l[0], auth_tok)
-        
+        self._kid_dct = None
+        self._parent_dct = None
     
+    @property
+    def kids(self):
+        if self._kid_dct is None:
+            self._kid_dct = {uuid: self.entity_factory.get(uuid) for uuid in self.kid_uuids}
+        return self._kid_dct
+    
+    @property
+    def parents(self):
+        if self._parent_dct is None:
+            self._parent_dct = {uuid: self.entity_factory.get(uuid) for uuid in self.parent_uuids}
+        return self._parent_dct
+
     def describe(self, prefix='', file=sys.stdout):
-        print(f"{prefix}{self.uuid}: "
+        print(f"{prefix}Dataset {self.uuid}: "
               f"{self.display_doi} "
               f"{self.data_types} "
               f"{self.status}",
               file=file)
         if self.kid_dataset_uuids:
-            assert self.kid_datasets, 'init_relatives() has not been called'
-            for kid in self.kid_datasets:
-                kid.describe(prefix=prefix+'    ', file=file)
-    
+            for kid in self.kid_dataset_uuids:
+                self.kids[kid].describe(prefix=prefix+'    ', file=file)
+
     def build_rec(self):
         """
         Returns a dict containing:
@@ -131,7 +118,13 @@ class Dataset(Entity):
         rec = {'uuid': self.uuid, 'display_doi': self.display_doi, 'status': self.status}
         assert len(self.data_types) == 1, f"More than one data_type: {self.data_types}"
         rec['data_type'] = self.data_types[0]
-        qa_kids = [kid_ds for kid_ds in self.kid_datasets if kid_ds.status == 'QA']
+        other_parent_uuids = [uuid for uuid in self.parent_uuids if uuid not in self.parent_dataset_uuids]
+        assert len(other_parent_uuids) == 1, 'More than one parent?'
+        samp = self.entity_factory.get(other_parent_uuids[0])
+        assert isinstance(samp, Sample), 'was expecting a sample?'
+        rec['sample_display_doi'] = samp.display_doi
+        rec['sample_hubmap_display_id'] = samp.hubmap_display_id
+        qa_kids = [self.kids[uuid] for uuid in self.kids if self.kids[uuid].status == 'QA']
         if any(qa_kids):
             if len(qa_kids) > 1:
                 rec['note'] = 'Multiple QA derived datasets'
@@ -149,6 +142,55 @@ class Dataset(Entity):
         return rec
             
 
+class Sample(Entity):
+    def __init__(self, prop_dct, entity_factory):
+        super().__init__(prop_dct, entity_factory)
+        assert prop_dct['entity_type'] == 'Sample', f"uuid {uuid} is a {prop_dct['entity_type']}"
+        self.display_doi = prop_dct['display_doi']
+        self.donor_display_doi = prop_dct['donor']['display_doi']
+        self.hubmap_display_id = prop_dct['hubmap_display_id']
+        self.donor_hubmap_display_id = prop_dct['donor']['hubmap_display_id']
+        self.donor_uuid = prop_dct['donor']['uuid']
+
+    def describe(self, prefix='', file=sys.stdout):
+        print(f"{prefix}Sample {self.uuid}: "
+              f"{self.display_doi} "
+              f"{self.hubmap_display_id}",
+              file=file)
+
+
+class EntityFactory(object):
+    def __init__(self, auth_tok=None):
+        assert auth_tok, 'auth_tok is required'
+        self.auth_tok = auth_tok
+    
+    def get(self, uuid):
+        """
+        Returns an entity of some kind
+        """
+        data = {'query': {'ids': {'values': [f'{uuid}']}}}
+        r = requests.post(f'{SEARCH_URL}/portal/search',
+                          data=json.dumps(data),
+                          headers={'Authorization': f'Bearer {self.auth_tok}',
+                                   'Content-Type': 'application/json'})
+        #print(f'query was {r.request.body}')
+        if r.status_code >= 300:
+            r.raise_for_status()
+        jsn = r.json()
+        assert len(jsn['hits']['hits']) == 1, f'More than one hit on uuid {uuid}'
+        hit = jsn['hits']['hits'][0]
+        assert hit['_id'] == uuid, f"uuid {uuid} gave back uuid {hit['_id']}"
+        prop_dct = hit['_source']
+        assert prop_dct['uuid'] == uuid, f"uuid {uuid} gave back inner uuid {prop_dct['uuid']}"
+        entity_type = prop_dct['entity_type']
+        if entity_type == 'Dataset':
+            return Dataset(prop_dct, self)
+        elif entity_type == 'Sample':
+            return Sample(prop_dct, self)
+        else:
+            return Entity(prop_dct, self)
+
+
 def get_uuid(rec):
     words = rec['data_path'].split('/')
     uuid = words[1]
@@ -163,19 +205,25 @@ def main():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("metadatatsv")
+    parser.add_argument("--out", help="name of the output .tsv file", required=True)
     args = parser.parse_args()
     auth_tok = input('auth_tok: ')
+    entity_factory = EntityFactory(auth_tok)
     in_df = pd.read_csv(args.metadatatsv, sep='\t')
     in_df['uuid'] = in_df.apply(get_uuid, axis=1)
+    out_recs = []
     for idx, row in in_df.iterrows():
         uuid = row['uuid']
-        ds = Dataset(uuid, auth_tok=auth_tok)
-        ds.init_relatives(auth_tok)
+        ds = entity_factory.get(uuid)
         ds.describe()
-        pprint(ds.build_rec())
-    #ds = Dataset(args.uuid, auth_tok)
-    #ds.describe()
-    
+        out_recs.append(ds.build_rec())
+    out_df = pd.DataFrame(out_recs).rename(columns={'sample_display_doi':'sample_doi',
+                                                    'sample_hubmap_display_id':'sample_display_id',
+                                                    'qa_child_uuid':'derived_uuid',
+                                                    'qa_child_display_doi':'derived_doi',
+                                                    'qa_child_data_type':'derived_data_type',
+                                                    'qa_child_status':'derived_status'})
+    out_df.to_csv(args.out, sep='\t', index=False)
     
 
 if __name__ == '__main__':
