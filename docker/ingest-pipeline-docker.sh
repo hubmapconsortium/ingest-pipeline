cd442c50f2f72d04d3e181639fbf16630dc724ae
yuanzhou <yuanzhou19@gmail.com>
2020-08-19 23:11:33 -0400

    Added DIR

--- docker/ingest-pipeline-docker.sh
+++ docker/ingest-pipeline-docker.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+function get_dir_of_this_script () {
+    # This function sets DIR to the directory in which this script itself is found.
+    # Thank you https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+    SCRIPT_SOURCE="${BASH_SOURCE[0]}"
+    while [ -h "$SCRIPT_SOURCE" ]; do # resolve $SCRIPT_SOURCE until the file is no longer a symlink
+        DIR="$( cd -P "$( dirname "$SCRIPT_SOURCE" )" >/dev/null 2>&1 && pwd )"
+        SCRIPT_SOURCE="$(readlink "$SCRIPT_SOURCE")"
+        [[ $SCRIPT_SOURCE != /* ]] && SCRIPT_SOURCE="$DIR/$SCRIPT_SOURCE" # if $SCRIPT_SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    DIR="$( cd -P "$( dirname "$SCRIPT_SOURCE" )" >/dev/null 2>&1 && pwd )"
+}
+
 # Remove newlines and leading/trailing slashes if present in that build_number file
 function export_build_num() {
     export INGEST_PIPELINE_BUILD_NUM=$(tr -d "\n\r" < ../build_number | xargs)
@@ -9,18 +21,21 @@ function export_build_num() {
 if [[ "$1" != "localhost" ]]; then
     echo "Unknown build environment '$1', specify: localhost"
 else
+    get_dir_of_this_script
+    echo 'DIR of script:' $DIR
+
     if [[ "$2" != "check" && "$2" != "config" && "$2" != "build" && "$2" != "start" && "$2" != "stop" && "$2" != "down" ]]; then
         echo "Unknown command '$2', specify one of the following: check|config|build|start|stop|down"
     else
         if [ "$2" = "check" ]; then
             # Bash array
             config_paths=(
-                '../ingest-pipeline/src/ingest-pipeline/instance/app.cfg'
+                '../src/ingest-pipeline/instance/app.cfg'
             )
 
             for pth in "${config_paths[@]}"; do
                 if [ ! -e $pth ]; then
-                    echo "Missing $pth"
+                    echo "Missing file (relative path to DIR of script) :$pth"
                     exit -1
                 fi
             done
