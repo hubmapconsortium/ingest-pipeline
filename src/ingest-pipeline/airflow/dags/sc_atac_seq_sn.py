313cac4abc81dddc1c3815a685c51ff100135cc6
Joel Welling <welling@psc.edu>
2020-08-20 20:58:14 -0400

    backport of sc_atac_seq_sn DAG from devel 68f6d90 with updates

--- src/ingest-pipeline/airflow/dags/sc_atac_seq_sn.py
+++ src/ingest-pipeline/airflow/dags/sc_atac_seq_sn.py
@@ -22,15 +22,17 @@ from hubmap_operators.common_operators import (
 
 import utils
 from utils import (
+    PIPELINE_BASE_DIR,
     decrypt_tok,
     find_pipeline_manifests,
-    get_absolute_workflows,
     get_cwltool_bin_path,
     get_dataset_uuid,
     get_parent_dataset_uuid,
     get_uuid_for_error,
     localized_assert_json_matches_schema as assert_json_matches_schema,
 )
+
+import cwltool  # used to find its path
 
 THREADS = 6  # to be used by the CWL worker
 
@@ -50,32 +52,44 @@ default_args = {
 }
 
 with DAG(
-        'bulk_atacseq',
+        'sc_atac_seq_sn',
         schedule_interval=None,
         is_paused_upon_creation=False,
         default_args=default_args,
         max_active_runs=4,
         user_defined_macros={'tmp_dir_path': utils.get_tmp_dir_path},
 ) as dag:
-    pipeline_name = 'bulk-atac-seq'
-    cwl_workflows = get_absolute_workflows(
-        [Path(pipeline_name, 'bulk-atac-seq-pipeline.cwl')],
-    )
+    pipeline_name = 'sn-atac-seq-pipeline'
+    cwl_workflows = [
+        Path(pipeline_name, 'create_snap_and_analyze.cwl'),
+        Path('portal-containers', 'scatac-csv-to-arrow.cwl'),
+    ]
+    cwl_workflows_absolute = [
+        PIPELINE_BASE_DIR / workflow
+        for workflow in cwl_workflows
+    ]
 
     def build_dataset_name(**kwargs):
-        return '{}__{}__{}'.format(
-            dag.dag_id,
+        return '{}__{}__{}'.format(dag.dag_id,
             kwargs['dag_run'].conf['parent_submission_id'],
             pipeline_name
         )
 
-    prepare_cwl1 = DummyOperator(task_id='prepare_cwl1')
+    prepare_cwl1 = DummyOperator(
+        task_id='prepare_cwl1'
+    )
+
+    prepare_cwl2 = DummyOperator(
+        task_id='prepare_cwl2'
+    )
 
     def build_cwltool_cmd1(**kwargs):
         ctx = kwargs['dag_run'].conf
         run_id = kwargs['run_id']
         tmpdir = Path(utils.get_tmp_dir_path(run_id))
+        print('tmpdir: ', tmpdir)
         data_dir = ctx['parent_lz_path']
+        print('data_dir: ', data_dir)
         cwltool_dir = get_cwltool_bin_path()
 
         command = [
@@ -89,11 +103,37 @@ with DAG(
             '--outdir',
             os.path.join(tmpdir, 'cwl_out'),
             '--parallel',
-            os.fspath(cwl_workflows[0]),
+            os.fspath(cwl_workflows_absolute[0]),
             '--sequence_directory',
             data_dir,
             '--threads',
             str(THREADS),
+        ]
+
+        command_str = ' '.join(shlex.quote(piece) for piece in command)
+        print('final command_str: {!r}'.format(command_str))
+        return command_str
+
+    def build_cwltool_cmd2(**kwargs):
+        ctx = kwargs['dag_run'].conf
+        run_id = kwargs['run_id']
+        tmpdir = Path(utils.get_tmp_dir_path(run_id))
+        print('tmpdir: ', tmpdir)
+        data_dir = ctx['parent_lz_path']
+        print('data_dir: ', data_dir)
+        cwltool_dir = get_cwltool_bin_path()
+
+        command = [
+            'env',
+            'PATH=%s:%s' % (cwltool_dir, os.environ['PATH']),
+            'TMPDIR=%s' % tmpdir,
+            'cwltool',
+            # trailing slash is deliberate
+            '--tmpdir-prefix={}/'.format(tmpdir / 'cwl-tmp'),
+            '--tmp-outdir-prefix={}/'.format(tmpdir / 'cwl-out-tmp'),
+            os.fspath(cwl_workflows_absolute[1]),
+            '--input_dir',
+            '.',
         ]
 
         command_str = ' '.join(shlex.quote(piece) for piece in command)
@@ -105,6 +145,11 @@ with DAG(
         python_callable=build_cwltool_cmd1,
     )
 
+    t_build_cmd2 = PythonOperator(
+        task_id='build_cmd2',
+        python_callable=build_cwltool_cmd2,
+    )
+
     t_pipeline_exec = BashOperator(
         task_id='pipeline_exec',
         bash_command=""" \
@@ -114,14 +159,37 @@ with DAG(
         """,
     )
 
+    t_make_arrow1 = BashOperator(
+        task_id='make_arrow1',
+        bash_command=""" \
+        tmp_dir={{tmp_dir_path(run_id)}} ; \
+        ds_dir="{{ti.xcom_pull(task_ids="send_create_dataset")}}" ; \
+        cd "$tmp_dir"/cwl_out ; \
+        {{ti.xcom_pull(task_ids='build_cmd2')}} >> $tmp_dir/session.log 2>&1 ; \
+        echo $?
+        """,
+        provide_context=True,
+    )
+
     t_maybe_keep_cwl1 = BranchPythonOperator(
         task_id='maybe_keep_cwl1',
         python_callable=utils.pythonop_maybe_keep,
         provide_context=True,
         op_kwargs={
-            'next_op': 'move_data',
+            'next_op': 'prepare_cwl2',
             'bail_op': 'set_dataset_error',
             'test_op': 'pipeline_exec',
+        },
+    )
+
+    t_maybe_keep_cwl2 = BranchPythonOperator(
+        task_id='maybe_keep_cwl2',
+        python_callable=utils.pythonop_maybe_keep,
+        provide_context=True,
+        op_kwargs={
+            'next_op': 'move_data',
+            'bail_op': 'set_dataset_error',
+            'test_op': 'make_arrow1',
         },
     )
 
@@ -134,7 +202,7 @@ with DAG(
             'http_conn_id': 'ingest_api_connection',
             'endpoint': '/datasets/derived',
             'dataset_name_callable': build_dataset_name,
-            "dataset_types": ["bulk_atacseq"],
+            "dataset_types": ["sn_atac_seq_sn"],
         },
     )
 
@@ -142,7 +210,7 @@ with DAG(
         task_id='set_dataset_error',
         python_callable=utils.pythonop_set_dataset_state,
         provide_context=True,
-        trigger_rule='one_success',
+        trigger_rule='all_done',
         op_kwargs={
             'dataset_uuid_callable': get_dataset_uuid,
             'http_conn_id': 'ingest_api_connection',
@@ -153,7 +221,7 @@ with DAG(
     )
 
     def send_status_msg(**kwargs):
-        retcode_ops = ['pipeline_exec', 'move_data']
+        retcode_ops = ['pipeline_exec', 'move_data', 'make_arrow1']
         retcodes = [
             int(kwargs['ti'].xcom_pull(task_ids=op))
             for op in retcode_ops
@@ -181,7 +249,7 @@ with DAG(
             md = {}
             files_for_provenance = [
                 __file__,
-                *cwl_workflows,
+                *cwl_workflows_absolute,
             ]
             if 'dag_provenance' in kwargs['dag_run'].conf:
                 md['dag_provenance'] = kwargs['dag_run'].conf['dag_provenance'].copy()
@@ -194,7 +262,7 @@ with DAG(
                 dag_prv.extend(utils.get_git_provenance_list(files_for_provenance))
                 md['dag_provenance_list'] = dag_prv
 
-            manifest_files = find_pipeline_manifests(*cwl_workflows)
+            manifest_files = find_pipeline_manifests(*cwl_workflows_absolute)
             md.update(
                 utils.get_file_metadata_dict(
                     ds_dir,
@@ -259,7 +327,9 @@ with DAG(
     (dag >> t_log_info >> t_create_tmpdir
      >> t_send_create_dataset >> t_set_dataset_processing
      >> prepare_cwl1 >> t_build_cmd1 >> t_pipeline_exec >> t_maybe_keep_cwl1
+     >> prepare_cwl2 >> t_build_cmd2 >> t_make_arrow1 >> t_maybe_keep_cwl2
      >> t_move_data >> t_send_status >> t_join)
     t_maybe_keep_cwl1 >> t_set_dataset_error
+    t_maybe_keep_cwl2 >> t_set_dataset_error
     t_set_dataset_error >> t_join
     t_join >> t_cleanup_tmpdir
