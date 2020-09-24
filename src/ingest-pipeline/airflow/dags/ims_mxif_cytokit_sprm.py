c6b7c4db866ab586869c5e20f04cc52bd1809898
Matt Ruffalo <matt.ruffalo@gmail.com>
2020-09-24 09:32:02 -0400

    Fix derived dataset type

--- src/ingest-pipeline/airflow/dags/ims_mxif_cytokit_sprm.py
+++ src/ingest-pipeline/airflow/dags/ims_mxif_cytokit_sprm.py
@@ -233,7 +233,7 @@ with DAG(
             "http_conn_id": "ingest_api_connection",
             "endpoint": "/datasets/derived",
             "dataset_name_callable": build_dataset_name,
-            "dataset_types": ["codex_cytokit"],
+            "dataset_types": ["ims_mxif_cytokit_sprm"],
         },
     )
 
