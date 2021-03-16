ab78a17e28834a8faae722f5dd28413115a7c836
Matt Ruffalo <matt.ruffalo@gmail.com>
2021-03-16 14:50:22 -0400

    cells_index DAG: remove indirection with DAG generator function
    This extra complexity is only necessary when multiple distinct DAGs are
    identical except for a few small parameter values.

--- src/ingest-pipeline/airflow/dags/cells_index.py
+++ src/ingest-pipeline/airflow/dags/cells_index.py
@@ -103,226 +103,221 @@ def get_data_directories(modality):
     else:
         return []
 
-def generate_cells_index_dag() -> DAG:
-    default_args = {
-        "owner": "hubmap",
-        "depends_on_past": False,
-        "start_date": datetime(2019, 1, 1),
-        "email": ["joel.welling@gmail.com"],
-        "email_on_failure": False,
-        "email_on_retry": False,
-        "retries": 1,
-        "retry_delay": timedelta(minutes=1),
-        "xcom_push": True,
-        "queue": utils.map_queue_name("general"),
-        "on_failure_callback": utils.create_dataset_state_error_callback(get_uuid_for_error),
-    }
+default_args = {
+    "owner": "hubmap",
+    "depends_on_past": False,
+    "start_date": datetime(2019, 1, 1),
+    "email": ["joel.welling@gmail.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+    "xcom_push": True,
+    "queue": utils.map_queue_name("general"),
+    "on_failure_callback": utils.create_dataset_state_error_callback(get_uuid_for_error),
+}
 
-    with DAG(
-        "cells_index",
-        schedule_interval=None,
-        is_paused_upon_creation=False,
-        default_args=default_args,
-        max_active_runs=4,
-        user_defined_macros={"tmp_dir_path": utils.get_tmp_dir_path},
-    ) as dag:
+with DAG(
+    "cells_index",
+    schedule_interval=None,
+    is_paused_upon_creation=False,
+    default_args=default_args,
+    max_active_runs=4,
+    user_defined_macros={"tmp_dir_path": utils.get_tmp_dir_path},
+) as dag:
 
-        cwl_workflows = get_absolute_workflows(
-            Path("cross-dataset-atac", "cross-dataset-atac.cwl"),
-            Path("cross-dataset-diffexpr", "cross-dataset-rna.cwl"),
-            Path("cross-dataset-codex", "cross-dataset-codex.cwl"),
-        )
+    cwl_workflows = get_absolute_workflows(
+        Path("cross-dataset-atac", "cross-dataset-atac.cwl"),
+        Path("cross-dataset-diffexpr", "cross-dataset-rna.cwl"),
+        Path("cross-dataset-codex", "cross-dataset-codex.cwl"),
+    )
 
-        prepare_cwl1 = DummyOperator(task_id="prepare_cwl1")
+    prepare_cwl1 = DummyOperator(task_id="prepare_cwl1")
 
-        prepare_cwl2 = DummyOperator(task_id="prepare_cwl2")
+    prepare_cwl2 = DummyOperator(task_id="prepare_cwl2")
 
-        prepare_cwl3 = DummyOperator(task_id="prepare_cwl3")
+    prepare_cwl3 = DummyOperator(task_id="prepare_cwl3")
 
-        def build_cwltool_cmd1(**kwargs):
-            run_id = kwargs["run_id"]
-            tmpdir = utils.get_tmp_dir_path(run_id)
-            print("tmpdir: ", tmpdir)
+    def build_cwltool_cmd1(**kwargs):
+        run_id = kwargs["run_id"]
+        tmpdir = utils.get_tmp_dir_path(run_id)
+        print("tmpdir: ", tmpdir)
 
-            data_dirs = get_data_directories("atac")
-            data_dirs = [data_dirs] if isinstance(data_dirs, str) else data_dirs
-            print("data_dirs: ", data_dirs)
+        data_dirs = get_data_directories("atac")
+        data_dirs = [data_dirs] if isinstance(data_dirs, str) else data_dirs
+        print("data_dirs: ", data_dirs)
 
-            command = [
-                *get_cwltool_base_cmd(tmpdir),
-                "--relax-path-checks",
-                "--debug",
-                "--outdir",
-                tmpdir / "cwl_out",
-                "--parallel",
-                cwl_workflows[0],
-            ]
+        command = [
+            *get_cwltool_base_cmd(tmpdir),
+            "--relax-path-checks",
+            "--debug",
+            "--outdir",
+            tmpdir / "cwl_out",
+            "--parallel",
+            cwl_workflows[0],
+        ]
 
-            for data_dir in data_dirs:
-                command.append("--data_directories")
-                command.append(data_dir)
+        for data_dir in data_dirs:
+            command.append("--data_directories")
+            command.append(data_dir)
 
-            return join_quote_command_str(command) if len(data_dirs) > 0 else ""
+        return join_quote_command_str(command) if len(data_dirs) > 0 else ""
 
-        def build_cwltool_cmd2(**kwargs):
-            run_id = kwargs["run_id"]
-            tmpdir = utils.get_tmp_dir_path(run_id)
-            print("tmpdir: ", tmpdir)
+    def build_cwltool_cmd2(**kwargs):
+        run_id = kwargs["run_id"]
+        tmpdir = utils.get_tmp_dir_path(run_id)
+        print("tmpdir: ", tmpdir)
 
-            data_dirs = get_data_directories("rna")
-            data_dirs = [data_dirs] if isinstance(data_dirs, str) else data_dirs
-            print("data_dirs: ", data_dirs)
+        data_dirs = get_data_directories("rna")
+        data_dirs = [data_dirs] if isinstance(data_dirs, str) else data_dirs
+        print("data_dirs: ", data_dirs)
 
-            command = [
-                *get_cwltool_base_cmd(tmpdir),
-                "--relax-path-checks",
-                "--debug",
-                "--outdir",
-                tmpdir / "cwl_out",
-                "--parallel",
-                cwl_workflows[1],
-            ]
+        command = [
+            *get_cwltool_base_cmd(tmpdir),
+            "--relax-path-checks",
+            "--debug",
+            "--outdir",
+            tmpdir / "cwl_out",
+            "--parallel",
+            cwl_workflows[1],
+        ]
 
-            for data_dir in data_dirs:
-                command.append("--data_directories")
-                command.append(data_dir)
+        for data_dir in data_dirs:
+            command.append("--data_directories")
+            command.append(data_dir)
 
-            return join_quote_command_str(command) if len(data_dirs) > 0 else ""
+        return join_quote_command_str(command) if len(data_dirs) > 0 else ""
 
-        def build_cwltool_cmd3(**kwargs):
-            run_id = kwargs["run_id"]
-            tmpdir = utils.get_tmp_dir_path(run_id)
-            print("tmpdir: ", tmpdir)
+    def build_cwltool_cmd3(**kwargs):
+        run_id = kwargs["run_id"]
+        tmpdir = utils.get_tmp_dir_path(run_id)
+        print("tmpdir: ", tmpdir)
 
-            data_dirs = get_data_directories("codex")
-            data_dirs = [data_dirs] if isinstance(data_dirs, str) else data_dirs
-            print("data_dirs: ", data_dirs)
+        data_dirs = get_data_directories("codex")
+        data_dirs = [data_dirs] if isinstance(data_dirs, str) else data_dirs
+        print("data_dirs: ", data_dirs)
 
-            command = [
-                *get_cwltool_base_cmd(tmpdir),
-                "--relax-path-checks",
-                "--debug",
-                "--outdir",
-                tmpdir / "cwl_out",
-                "--parallel",
-                cwl_workflows[2],
-            ]
+        command = [
+            *get_cwltool_base_cmd(tmpdir),
+            "--relax-path-checks",
+            "--debug",
+            "--outdir",
+            tmpdir / "cwl_out",
+            "--parallel",
+            cwl_workflows[2],
+        ]
 
-            for data_dir in data_dirs:
-                command.append("--data_directories")
-                command.append(data_dir)
+        for data_dir in data_dirs:
+            command.append("--data_directories")
+            command.append(data_dir)
 
-            return join_quote_command_str(command) if len(data_dirs) > 0 else ""
+        return join_quote_command_str(command) if len(data_dirs) > 0 else ""
 
-        t_build_cmd1 = PythonOperator(
-            task_id="build_cmd1",
-            python_callable=build_cwltool_cmd1,
-            provide_context=True,
-        )
+    t_build_cmd1 = PythonOperator(
+        task_id="build_cmd1",
+        python_callable=build_cwltool_cmd1,
+        provide_context=True,
+    )
 
-        t_build_cmd2 = PythonOperator(
-            task_id="build_cmd2",
-            python_callable=build_cwltool_cmd2,
-            provide_context=True,
-        )
+    t_build_cmd2 = PythonOperator(
+        task_id="build_cmd2",
+        python_callable=build_cwltool_cmd2,
+        provide_context=True,
+    )
 
-        t_build_cmd3 = PythonOperator(
-            task_id="build_cmd3",
-            python_callable=build_cwltool_cmd3,
-            provide_context=True,
-        )
+    t_build_cmd3 = PythonOperator(
+        task_id="build_cmd3",
+        python_callable=build_cwltool_cmd3,
+        provide_context=True,
+    )
 
-        t_pipeline_exec1 = BashOperator(
-            task_id="pipeline_exec",
-            bash_command=""" \
-            tmp_dir={{tmp_dir_path(run_id)}} ; \
-            {{ti.xcom_pull(task_ids='build_cmd1')}} > $tmp_dir/session.log 2>&1 ; \
-            echo $?
-            """,
-        )
+    t_pipeline_exec1 = BashOperator(
+        task_id="pipeline_exec",
+        bash_command=""" \
+        tmp_dir={{tmp_dir_path(run_id)}} ; \
+        {{ti.xcom_pull(task_ids='build_cmd1')}} > $tmp_dir/session.log 2>&1 ; \
+        echo $?
+        """,
+    )
 
-        t_pipeline_exec2 = BashOperator(
-            task_id="pipeline_exec",
-            bash_command=""" \
-            tmp_dir={{tmp_dir_path(run_id)}} ; \
-            {{ti.xcom_pull(task_ids='build_cmd1')}} > $tmp_dir/session.log 2>&1 ; \
-            echo $?
-            """,
-        )
+    t_pipeline_exec2 = BashOperator(
+        task_id="pipeline_exec",
+        bash_command=""" \
+        tmp_dir={{tmp_dir_path(run_id)}} ; \
+        {{ti.xcom_pull(task_ids='build_cmd1')}} > $tmp_dir/session.log 2>&1 ; \
+        echo $?
+        """,
+    )
 
-        t_pipeline_exec3 = BashOperator(
-            task_id="pipeline_exec",
-            bash_command=""" \
-            tmp_dir={{tmp_dir_path(run_id)}} ; \
-            {{ti.xcom_pull(task_ids='build_cmd1')}} > $tmp_dir/session.log 2>&1 ; \
-            echo $?
-            """,
-        )
+    t_pipeline_exec3 = BashOperator(
+        task_id="pipeline_exec",
+        bash_command=""" \
+        tmp_dir={{tmp_dir_path(run_id)}} ; \
+        {{ti.xcom_pull(task_ids='build_cmd1')}} > $tmp_dir/session.log 2>&1 ; \
+        echo $?
+        """,
+    )
 
-        t_maybe_keep_cwl1 = BranchPythonOperator(
-            task_id="maybe_keep_cwl1",
-            python_callable=utils.pythonop_maybe_keep,
-            provide_context=True,
-            op_kwargs={
-                "next_op": "prepare_cwl2",
-                "bail_op": "set_dataset_error",
-                "test_op": "pipeline_exec",
-            },
-        )
+    t_maybe_keep_cwl1 = BranchPythonOperator(
+        task_id="maybe_keep_cwl1",
+        python_callable=utils.pythonop_maybe_keep,
+        provide_context=True,
+        op_kwargs={
+            "next_op": "prepare_cwl2",
+            "bail_op": "set_dataset_error",
+            "test_op": "pipeline_exec",
+        },
+    )
 
-        t_maybe_keep_cwl2 = BranchPythonOperator(
-            task_id="maybe_keep_cwl2",
-            python_callable=utils.pythonop_maybe_keep,
-            provide_context=True,
-            op_kwargs={
-                "next_op": "prepare_cwl3",
-                "bail_op": "set_dataset_error",
-                "test_op": "convert_for_ui",
-            },
-        )
+    t_maybe_keep_cwl2 = BranchPythonOperator(
+        task_id="maybe_keep_cwl2",
+        python_callable=utils.pythonop_maybe_keep,
+        provide_context=True,
+        op_kwargs={
+            "next_op": "prepare_cwl3",
+            "bail_op": "set_dataset_error",
+            "test_op": "convert_for_ui",
+        },
+    )
 
-        t_maybe_keep_cwl3 = BranchPythonOperator(
-            task_id="maybe_keep_cwl3",
-            python_callable=utils.pythonop_maybe_keep,
-            provide_context=True,
-            op_kwargs={
-                "next_op": "move_data",
-                "bail_op": "set_dataset_error",
-                "test_op": "convert_for_ui_2",
-            },
-        )
+    t_maybe_keep_cwl3 = BranchPythonOperator(
+        task_id="maybe_keep_cwl3",
+        python_callable=utils.pythonop_maybe_keep,
+        provide_context=True,
+        op_kwargs={
+            "next_op": "move_data",
+            "bail_op": "set_dataset_error",
+            "test_op": "convert_for_ui_2",
+        },
+    )
 
 
-        t_log_info = LogInfoOperator(task_id="log_info")
-        t_join = JoinOperator(task_id="join")
-        t_create_tmpdir = CreateTmpDirOperator(task_id="create_tmpdir")
-        t_cleanup_tmpdir = CleanupTmpDirOperator(task_id="cleanup_tmpdir")
-        t_move_data = MoveDataOperator(task_id="move_data")
+    t_log_info = LogInfoOperator(task_id="log_info")
+    t_join = JoinOperator(task_id="join")
+    t_create_tmpdir = CreateTmpDirOperator(task_id="create_tmpdir")
+    t_cleanup_tmpdir = CleanupTmpDirOperator(task_id="cleanup_tmpdir")
+    t_move_data = MoveDataOperator(task_id="move_data")
 
-        (
-            dag
-            >> t_log_info
-            >> t_create_tmpdir
-            >> prepare_cwl1
-            >> t_build_cmd1
-            >> t_pipeline_exec1
-            >> t_maybe_keep_cwl1
-            >> prepare_cwl2
-            >> t_build_cmd2
-            >> t_pipeline_exec2
-            >> t_maybe_keep_cwl2
-            >> prepare_cwl3
-            >> t_build_cmd3
-            >> t_pipeline_exec3
-            >> t_maybe_keep_cwl3
-            >> t_move_data
-            >> t_join
-        )
-        t_maybe_keep_cwl1 >> t_join
-        t_maybe_keep_cwl2 >> t_join
-        t_maybe_keep_cwl3 >> t_join
-        t_join >> t_cleanup_tmpdir
-
-    return dag
-
-globals()["cells_index"] = generate_cells_index_dag()
+    (
+        dag
+        >> t_log_info
+        >> t_create_tmpdir
+        >> prepare_cwl1
+        >> t_build_cmd1
+        >> t_pipeline_exec1
+        >> t_maybe_keep_cwl1
+        >> prepare_cwl2
+        >> t_build_cmd2
+        >> t_pipeline_exec2
+        >> t_maybe_keep_cwl2
+        >> prepare_cwl3
+        >> t_build_cmd3
+        >> t_pipeline_exec3
+        >> t_maybe_keep_cwl3
+        >> t_move_data
+        >> t_join
+    )
+    t_maybe_keep_cwl1 >> t_join
+    t_maybe_keep_cwl2 >> t_join
+    t_maybe_keep_cwl3 >> t_join
+    t_join >> t_cleanup_tmpdir
