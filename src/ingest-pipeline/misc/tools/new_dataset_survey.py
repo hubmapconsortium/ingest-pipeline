170c8f3c9a87b279a40552855fa5256a345a4182
Hubmap Hive <hive@hivevm191.psc.edu>
2021-03-20 13:59:00 -0400

    working version of new_dataset_survey.py, with sorted rows and columns.

--- src/ingest-pipeline/misc/tools/new_dataset_survey.py
+++ src/ingest-pipeline/misc/tools/new_dataset_survey.py
@@ -10,17 +10,66 @@ import pandas as pd
 from survey import Entity, Dataset, Sample, EntityFactory, is_uuid
 
 
-def detect_metadatatsv(ds):
+#
+# Large negative numbers move columns left, large positive numbers move them right.
+# Columns for which no weight is given end up with weight 0, so they sort to the
+# middle alphabetically.
+#
+COLUMN_SORT_WEIGHTS = {
+    'note':10,
+    'n_md_recs': 9,
+    'has_metadata': 8,
+    'has_data': 7,
+    'group_name': -10,
+    'data_types': -9,
+    'uuid': -8,
+    'hubmap_id': -7,
+}
+
+
+#
+# Column labels to be used as keys in sorting rows
+#
+ROW_SORT_KEYS = ['group_name', 'data_types', 'uuid']
+
+
+def column_sorter(col_l):
+    sort_me = [((COLUMN_SORT_WEIGHTS[key] if key in COLUMN_SORT_WEIGHTS else 0), key) for key in col_l]
+    return [key for wt, key in sorted(sort_me)]
+
+
+def detect_otherdata(ds):
     """
     Returns (True, nrecs) if there is a useable metadata.tsv file in the dataset
     top level directory, or (False, 0) otherwise
     """
+    for path in ds.full_path.glob('**/*'):
+        if path.is_file() and path.suffix != '.tsv':
+            return True
+    return False
+
+
+def detect_metadatatsv(ds):
+    """
+    Returns True if there are non-tsv files in or below the dataset top level
+    directory, or False otherwise.
+    """
     for path in ds.full_path.glob('*metadata.tsv'):
         md_df = pd.read_csv(path, sep='\t')
-        print(len(md_df), 'assay_type' in md_df.columns)
         if 'assay_type' in md_df.columns:
             return (True, len(md_df))
     return (False, 0)
+
+
+def data_type_resolver(row):
+    if isinstance(row["data_types_x"], str) and isinstance(row["data_types_y"], str):
+        text_rep = row["data_types_y"]
+        if text_rep[0] == '[' and text_rep[-1] == ']':
+            text_rep = text_rep[1:-1]
+        text_rep = text_rep.strip("'")
+        if text_rep == row["data_types_x"]:
+            return row["data_types_x"]
+    return "????"
 
 
 def main():
@@ -74,6 +123,7 @@ def main():
         new_uuids = ds.all_uuids()
         rec = ds.build_rec()
         rec['has_metadata'], rec['n_md_recs'] = detect_metadatatsv(ds)
+        rec['has_data'] = detect_otherdata(ds)
         if any([uuid in known_uuids for uuid in new_uuids]):
             rec['note'] = 'UUID COLLISION! '
         known_uuids = known_uuids.union(new_uuids)
@@ -86,11 +136,29 @@ def main():
                                                     'qa_child_status':'derived_status'})
     if in_df is not None:
         out_df = out_df.merge(in_df, left_on='uuid', right_on=uuid_key)
-    # out_df.to_csv(args.out, sep='\t', index=False,
-    #               columns=['uuid', 'group_name', 'display_doi', 'status', 'data_types',
-    #                        'has_metadata', 'n_md_recs'])
-    out_df.to_csv(args.out, sep='\t', index=False)
-    
+
+    # Some cleanup on out_df before we save it
+    drop_list = []
+    rename_d = {}
+    for col in out_df.columns:
+        if col.startswith('derived_'):
+            drop_list.append(col)
+    if 'group_name' in out_df.columns and 'organization' in out_df.columns:
+        drop_list.append('organization')
+    if 'display_doi' in out_df.columns and 'hubmap_id' in out_df.columns:
+        assert (out_df['display_doi'] == out_df['hubmap_id']).all(), 'display_doi and hubmap_id do not match?'
+        drop_list.append('display_doi')
+    if 'data_types_x' in out_df.columns and 'data_types_y' in out_df.columns:
+        out_df['data_types'] = out_df[['data_types_x', 'data_types_y']].apply(data_type_resolver, axis=1)
+        drop_list.extend(['data_types_x', 'data_types_y'])
+    out_df = out_df.drop(drop_list, axis=1)
+
+    out_df = out_df.sort_values(ROW_SORT_KEYS, axis=0)
+
+    out_df.to_csv(args.out, sep='\t', index=False,
+                  columns=column_sorter([elt for elt in out_df.columns])
+                  )
+
 
 if __name__ == '__main__':
     main()
