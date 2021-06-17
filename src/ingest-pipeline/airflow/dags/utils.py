2c03c7682446f92c03cba17ac7f18cff28b5168a
Zhou (Joe) Yuan <yuanzhou19@gmail.com>
2021-06-16 22:44:49 -0400

    Use join() instead of os.path.join()

--- src/ingest-pipeline/airflow/dags/utils.py
+++ src/ingest-pipeline/airflow/dags/utils.py
@@ -936,7 +936,7 @@ def make_send_status_msg_function(
             ###################################################################################
             # Added by Zhou 6/16/2021 for registering thumbnail image
             dataset_full_path = dataset_lz_path_fun(**kwargs)
-            thumbnail_image_full_path = os.path.join(dataset_full_path, 'extra/thumbnail.jpg')
+            thumbnail_image_full_path = join(dataset_full_path, 'extra/thumbnail.jpg')
             if exists(thumbnail_image_full_path):
                  md['thumbnail_image'] = thumbnail_image_full_path
             ###################################################################################
