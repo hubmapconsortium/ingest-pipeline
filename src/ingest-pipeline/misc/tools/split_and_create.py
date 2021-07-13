f89ebce8d4aeedca0ed21f8cadd82cafc7dac76b
Hubmap Hive <hive@hivevm191.psc.edu>
2021-07-13 18:03:28 -0400

    Version of split_and_create.py that works for at least one special case.

--- src/ingest-pipeline/misc/tools/split_and_create.py
+++ src/ingest-pipeline/misc/tools/split_and_create.py
@@ -13,13 +13,11 @@ from hubmap_commons.globus_groups import get_globus_groups_info
 
 from survey import (Entity, Dataset, Sample, EntityFactory,
                     ROW_SORT_KEYS, column_sorter, is_uuid,
-                    parse_text_list)
+                    parse_text_list, ENDPOINTS)
 
 
-#SOURCE_PATH = Path("/hive/hubmap/data/protected/University of California San Diego TMC/"
-#                   "1b1b6bb2e8d1c91fe7ab411348304822/1b1b6bb2e8d1c91fe7ab411348304822-metadata.tsv")
-#SOURCE_UUID = "1b1b6bb2e8d1c91fe7ab411348304822"
-SOURCE_UUID = "6e9e4da81edfe046b3acf3b49e7593c0"
+FROZEN_DF_FNAME = 'frozen_source_df.tsv'
+
 
 def get_canonical_assay_type(row, entity_factory, default_type):
     try:
@@ -64,14 +62,25 @@ def populate(idx, row, source_df, source_entity, entity_factory):
     row['data_path'] = '.'
     canonical_assay_type = row['canonical_assay_type']
     row['assay_type'] = {'SNARE-Seq2-AC': 'SNARE-seq2',
-                         'SNARE-Seq2-R': 'SNARE2-RNAseq'}.get(canonical_assay_type,
-                                                              canonical_assay_type)
+                         'SNARE-Seq2-R': 'SNARE2-RNAseq',
+                         'SNAREseq': 'SNARE2-RNAseq'}.get(canonical_assay_type,
+                                                       canonical_assay_type)
     row_df = pd.DataFrame([row])
     row_df = row_df.drop(columns=['canonical_assay_type', 'new_uuid'])
     row_df.to_csv(kid_path / f'{uuid}-metadata.tsv', header=True, sep='\t', index=False)
-    (kid_path / 'extras').mkdir(0o770, exist_ok=True)
-    #copytree(source_entity.full_path / 'extras', kid_path / 'extras',
-    #         dirs_exist_ok=True)
+    extras_path = kid_path / 'extras'
+    if extras_path.exists():
+        assert extras_path.is_dir(), f'{extras_path} is not a directory'
+    else:
+        source_extras_path = source_entity.full_path / 'extras'
+        if source_extras_path.exists():
+            copytree(source_extras_path, extras_path)
+        else:
+            extras_path.mkdir(0o770)
+    source_data_path = source_entity.full_path / old_data_path
+    for elt in source_data_path.glob('*'):
+        elt.rename(kid_path / elt.name)
+    # print(f'mv "{source_entity.full_path / old_data_path}/"* "{kid_path}"')
     print(f"{old_data_path} -> {uuid} -> full path: {kid_path}")
     
     # c_p = row['contributors_path']
@@ -124,36 +133,68 @@ def main():
     """
     main
     """
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("uuid_txt",
-    #                     help="input .txt file containing uuids or .csv or .tsv file with uuid column")
-    # parser.add_argument("--out", help="name of the output .tsv file", required=True)
-    # parser.add_argument("--notes", action="append",
-    #                     help=("merge dataset notes from this csv/tsv file"
-    #                           " (may be repeated)."))
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("uuid",
+                        help="input .txt file containing uuids or .csv or .tsv file with uuid column")
+    parser.add_argument("--stop", help=f"stop after creating child uuids and writing {FROZEN_DF_FNAME}",
+                        action="store_true", )
+    parser.add_argument("--unstop", help=f"do not create child uuids; read {FROZEN_DF_FNAME} and continue",
+                        action="store_true")
+    parser.add_argument("--instance", help=f"instance to use. One of {ENDPOINTS.keys()}",
+                        default = 'PROD')
+    args = parser.parse_args()
+
+    if args.stop and args.unstop:
+        parser.error("--stop and --unstop are mutually exclusive")
+    if len(args.uuid) == 32:
+        try:
+            int(args.uuid, base=16)
+        except ValueError:
+            parser.error(f"{args.uuid} doesn't look like a uuid")
+    else:
+        parser.error(f"{args.uuid} is the wrong length to be a uuid")
+    if args.instance not in ENDPOINTS.keys():
+        parser.error(f"{args.instance} is not a known instance")
+    source_uuid = args.uuid
+    instance = args.instance
+    if args.stop:
+        mode = 'stop'
+    elif args.unstop:
+        mode = 'unstop'
+    else:
+        mode = 'all'
+
     auth_tok = input('auth_tok: ')
     entity_factory = EntityFactory(auth_tok, instance='PROD')
 
-    source_entity = entity_factory.get(SOURCE_UUID)
-    source_metadata_files = [elt for elt in source_entity.full_path.glob('*metadata.tsv')]
-    assert len(source_metadata_files) == 1, f'Too many netadata files in {source_entity.full_path}'
-    # source_df = pd.read_csv(source_metadata_files[0], sep='\t')
-    # assert isinstance(source_entity.data_types, str)
-    # source_df['canonical_assay_type'] = source_df.apply(get_canonical_assay_type,
-    #                                                     axis=1,
-    #                                                     entity_factory=entity_factory,
-    #                                                     default_type=source_entity.data_types)
-    # source_df['new_uuid'] = source_df.apply(create_new_uuid, axis=1,
-    #                                         source_entity=source_entity,
-    #                                         entity_factory=entity_factory)
-    # print(source_df[['data_path', 'canonical_assay_type', 'new_uuid']])
-    # source_df.to_csv('frozen_source_df.tsv', sep='\t', header=True, index=False)
-    # sys.exit('done')
+    print(f'Decomposing {source_uuid}')
+    source_entity = entity_factory.get(source_uuid)
+    if mode in ['all', 'stop']:
+        source_metadata_files = [elt for elt in source_entity.full_path.glob('*metadata.tsv')]
+        assert len(source_metadata_files) == 1, f'Too many netadata files in {source_entity.full_path}'
+        source_df = pd.read_csv(source_metadata_files[0], sep='\t')
+        assert isinstance(source_entity.data_types, str)
+        source_df['canonical_assay_type'] = source_df.apply(get_canonical_assay_type,
+                                                            axis=1,
+                                                            entity_factory=entity_factory,
+                                                            default_type=source_entity.data_types)
+        source_df['new_uuid'] = source_df.apply(create_new_uuid, axis=1,
+                                                source_entity=source_entity,
+                                                entity_factory=entity_factory)
+        print(source_df[['data_path', 'canonical_assay_type', 'new_uuid']])
+        source_df.to_csv(FROZEN_DF_FNAME, sep='\t', header=True, index=False)
+        print(f'wrote {FROZEN_DF_FNAME}')
 
-    source_df = pd.read_csv('frozen_source_df.tsv', sep='\t')
-    for idx, row in source_df.iterrows():
-        populate(idx, row, source_df, source_entity, entity_factory)
+    if mode == 'stop':
+        sys.exit('done')
+
+    if mode == 'unstop':
+        source_df = pd.read_csv(FROZEN_DF_FNAME, sep='\t')
+        print(f'read {FROZEN_DF_FNAME}')
+
+    if mode in ['all', 'unstop']:
+        for idx, row in source_df.iterrows():
+            populate(idx, row, source_df, source_entity, entity_factory)
 
 
 if __name__ == '__main__':
