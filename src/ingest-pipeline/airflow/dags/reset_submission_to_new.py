1b6a21e19c31d5984c86a6d2dfdce4608b736e5a
Hubmap Hive <hive@hivevm191.psc.edu>
2022-05-17 18:47:45 -0400

    Merge changes to reset_dataset_to_new DAG

--- src/ingest-pipeline/airflow/dags/reset_submission_to_new.py
+++ src/ingest-pipeline/airflow/dags/reset_submission_to_new.py
@@ -19,9 +19,17 @@ import utils
 from utils import localized_assert_json_matches_schema as assert_json_matches_schema
 
 UUIDS_TO_RESET = [
-    '2c467ffa1d01c41effb7057d7d329c8f',
-    '48c8dd2ad06aa23e36c095c9088a4913',
-    '08ee9f5575339641eb9f8fb17cc1d1bd'
+    #'2c467ffa1d01c41effb7057d7d329c8f',
+    #'48c8dd2ad06aa23e36c095c9088a4913',
+    #'08ee9f5575339641eb9f8fb17cc1d1bd'
+    #"0eb5e457b4855ce28531bc97147196b6",  # HCA scRNAseq-10x-v2
+    "33874bc3c95b6b41cbc387d2826d88eb",  # San Diego scRNAseq-SNARE
+    #"4758c75194e26ef598ec611916042f51",  # sample Upload on DEV
+    #"060dfa0fdf2b840864f62d2cd1a7a456",  # GE CellDIVE
+    #"488f364142c308a9692e0b529f6697dd",  # GUDMAP scRNAseq disguised as UCSD 
+    #'2c467ffa1d01c41effb7057d7d329c8f',
+    #'48c8dd2ad06aa23e36c095c9088a4913',
+    #'08ee9f5575339641eb9f8fb17cc1d1bd'
     ]
 
 
