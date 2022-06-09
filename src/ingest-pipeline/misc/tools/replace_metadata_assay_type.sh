8c7697b963a60a78b26440bea33247fb25b32ee7
Joel Welling <welling@psc.edu>
2022-06-09 15:15:18 -0400

    Added checking of file and directory protections, test that change happened

--- src/ingest-pipeline/misc/tools/replace_metadata_assay_type.sh
+++ src/ingest-pipeline/misc/tools/replace_metadata_assay_type.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -ex
 
-old_assay_name='MALDI-IMS-neg'
-new_assay_name='MALDI-IMS'
+set -e  # really exit on error
+
+old_assay_name='LC-MS (metabolomics)'
+new_assay_name='LC-MS'
 uuid=$1
 echo $uuid
 echo `basename "$PWD"`
@@ -13,6 +15,19 @@ fi
 fname=`ls *metadata.tsv | head -1`
 echo $fname
 
+dir_prot=`stat -c '%a' .`
+chmod u+w .
 mkdir -p extras
+extras_prot=`stat -c '%a' extras`
+chmod u+w extras
+prot=`stat -c '%a' $fname`
 mv $fname extras/${fname}.orig
 sed "s/${old_assay_name}/${new_assay_name}/" < extras/${fname}.orig > $fname
+chmod $prot $fname
+chmod $extras_prot extras
+chmod $dir_prot .
+cmp --silent $fname extras/${fname}.orig
+if [ ! $? ] ; then
+    echo "The edit to metadata made no change!"
+    exit -1
+fi
