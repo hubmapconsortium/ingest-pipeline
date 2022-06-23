d7c83ca1fc43de69757b892d8f9ba9934e964df6
Joel Welling <welling@psc.edu>
2022-06-23 16:42:43 -0400

    Response to review comments

--- src/ingest-pipeline/misc/tools/replace_metadata_assay_type.sh
+++ src/ingest-pipeline/misc/tools/replace_metadata_assay_type.sh
@@ -2,10 +2,9 @@
 
 set -e  # really exit on error
 
-#old_assay_name='LC-MS (metabolomics)'
-#new_assay_name='LC-MS'
 old_assay_name='LC-MS/MS (label-free proteomics)'
 new_assay_name='LC-MS Bottom-Up'
+
 uuid=$1
 echo $uuid
 echo `basename "$PWD"`
@@ -17,15 +16,15 @@ fi
 fname=`ls *metadata.tsv | head -1`
 echo $fname
 
-dir_prot=`stat -c '%a' .`
+dir_prot="$(stat -c '%a' .)"
 chmod u+w .
 mkdir -p extras
-extras_prot=`stat -c '%a' extras`
+extras_prot="$(stat -c '%a' extras)"
 chmod u+w extras
-prot=`stat -c '%a' $fname`
+prot="$(stat -c '%a' $fname)"
 mv $fname extras/${fname}.orig
 ESCAPED_REPLACE=$(printf '%s\n' "$new_assay_name" | sed -e 's/[\/&]/\\&/g')
-ESCAPED_KEYWORD=$(printf '%s\n' "$old_assay_name" | sed -e 's/[]\/$*.^[]/\\&/g');
+ESCAPED_KEYWORD=$(printf '%s\n' "$old_assay_name" | sed -e 's/[]\/$*.^[]/\\&/g')
 sed "s/${ESCAPED_KEYWORD}/${ESCAPED_REPLACE}/" < extras/${fname}.orig > $fname
 chmod $prot $fname
 chmod $extras_prot extras
