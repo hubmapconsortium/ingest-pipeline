8e4c8d5573f88bf6a3382f31b7ea541c839927dc
Hubmap Hive <hive@gpu002.pvt.hive.psc.edu>
2022-07-01 22:27:35 -0400

    Apparently working support for DEV GPU on gpu002

---
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_prod.sh
@@ -0,0 +1,9 @@
+#set airflow environment
+HM_AF_CONFIG=/hive/users/hive/hubmap/hivevm193-prod/ingest-pipeline/src/ingest-pipeline/airflow/airflow.cfg
+HM_AF_HOME=/hive/users/hive/hubmap/hivevm193-prod/ingest-pipeline/src/ingest-pipeline/airflow
+
+HM_AF_CONN_INGEST_API_CONNECTION=http://hivevm193.psc.edu:7777/
+HM_AF_CONN_UUID_API_CONNECTION=http://https%3a%2f%2fuuid.api.hubmapconsortium.org/
+HM_AF_CONN_CELLS_API_CONNECTION=http://https%3a%2f%2fcells.api.hubmapconsortium.org/
+HM_AF_CONN_SEARCH_API_CONNECTION=http://https%3a%2f%2fsearch.api.hubmapconsortium.org/
+HM_AF_CONN_ENTITY_API_CONNECTION=http://https%3a%2f%2fentity.api.hubmapconsortium.org/
