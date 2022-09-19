feffcf91f3761254b56bb20358d4e1145a19f5d7
David Betancur <dbetancur@psc.edu>
2022-09-19 17:04:29 -0400

    playing around with custom authentication plugin

--- src/ingest-pipeline/airflow/plugins/globus_auth/globus_auth_rbac.py
+++ src/ingest-pipeline/airflow/plugins/globus_auth/globus_auth_rbac.py
@@ -130,36 +130,12 @@ class CustomOAuthView(AuthOAuthView):
                 raise Exception('User does not have correct group assignments')
 
             user = self.appbuilder.sm.auth_user_oauth(user_info)
+            if not user:
+                user = models.User(
+                    username=username,
+                    email=email,
+                    is_superuser=False)
 
-            # resp = self.appbuilder.sm.oauth_remotes[provider].authorized_response()
-            # if resp is None:
-            #     flash(u"You denied the request to sign in.", "warning")
-            #     return redirect(self.appbuilder.get_url_for_login)
-            # log.debug("OAUTH Authorized resp: {0}".format(resp))
-            # # Retrieves specific user info from the provider
-            # try:
-            #     self.appbuilder.sm.set_oauth_session(provider, resp)
-            #     userinfo = self.appbuilder.sm.oauth_user_info(provider, resp)
-            # except Exception as e:
-            #     log.error("Error returning OAuth user info: {0}".format(e))
-            #     user = None
-            # else:
-            #     log.debug("User info retrieved from {0}: {1}".format(provider, userinfo))
-            #     # User email is not whitelisted
-            #     if provider in self.appbuilder.sm.oauth_whitelists:
-            #         whitelist = self.appbuilder.sm.oauth_whitelists[provider]
-            #         allow = False
-            #         for e in whitelist:
-            #             if re.search(e, userinfo["email"]):
-            #                 allow = True
-            #                 break
-            #         if not allow:
-            #             flash(u"You are not authorized.", "warning")
-            #             return redirect(self.appbuilder.get_url_for_login)
-            #     else:
-            #         log.debug("No whitelist for OAuth provider")
-            #     user = self.appbuilder.sm.auth_user_oauth(userinfo)
-            #
             if user is None:
                 flash(as_unicode(self.invalid_login_message), "warning")
                 return redirect(self.appbuilder.get_url_for_login)
