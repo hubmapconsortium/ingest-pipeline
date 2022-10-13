fbfe26e4ff9e65f08f8a3ce7a0b5af56a4bdd3b4
Joel Welling <welling@psc.edu>
2022-10-13 14:12:51 -0400

    Importing updated config scripts developed during release_1.9 testing

--- src/ingest-pipeline/misc/tools/airflow_environments/env_centos_7_test.sh
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_centos_7_test.sh
@@ -2,11 +2,11 @@
 
 # HM_AF_METHOD must be one of venv, module_conda, or conda
 # HM_AF_ENV_NAME must be the name of the conda environment or the full path to the venv dir
-HM_AF_METHOD='conda'
-HM_AF_ENV_NAME='condaEnv_centos_8_python_3.6_dev'
+HM_AF_METHOD='venv'
+HM_AF_ENV_NAME='/hive/users/hive/hubmap/hivevm192-test/venv'
 
 PARENTDIR="$(dirname "$(readlink -f "$0")")"
-. "${PARENTDIR}/airflow_environments/env_dev.sh"
+. "${PARENTDIR}/airflow_environments/env_test.sh"
 
 
 
