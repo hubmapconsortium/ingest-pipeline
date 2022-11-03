e293393f9161e688e7bcc342bf988a71909dbdf0
David Betancur <dbetancur@psc.edu>
2022-11-03 17:13:37 -0400

    Bugfix url parsing for new search api v3

--- src/ingest-pipeline/misc/tools/airflow_environments/env_dev.sh
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_dev.sh
@@ -5,5 +5,5 @@ HM_AF_HOME=/hive/users/hive/hubmap/hivevm191-dev/ingest-pipeline/src/ingest-pipe
 HM_AF_CONN_INGEST_API_CONNECTION=http://hivevm191.psc.edu:7777/
 HM_AF_CONN_UUID_API_CONNECTION=http://https%3a%2f%2fuuid-api.dev.hubmapconsortium.org/
 HM_AF_CONN_CELLS_API_CONNECTION=http://https%3a%2f%2fcells-api.dev.hubmapconsortium.org/
-HM_AF_CONN_SEARCH_API_CONNECTION=http://https%3a%2f%2fsearch-api.dev.hubmapconsortium.org/v3/
+HM_AF_CONN_SEARCH_API_CONNECTION=http://https%3a%2f%2fsearch-api.dev.hubmapconsortium.org%2fv3/
 HM_AF_CONN_ENTITY_API_CONNECTION=http://https%3a%2f%2fentity-api.dev.hubmapconsortium.org/
