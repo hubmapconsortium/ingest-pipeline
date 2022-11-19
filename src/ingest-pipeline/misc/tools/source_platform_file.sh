b299167cc7255234849c1c13ef1f3d051c963b56
Joel Welling <welling@psc.edu>
2022-11-18 20:42:58 -0500

    . source_platform_file.sh instead of sourcing the platform file

--- src/ingest-pipeline/misc/tools/source_platform_file.sh
+++ src/ingest-pipeline/misc/tools/source_platform_file.sh
@@ -8,7 +8,6 @@ fi
 # Path from top level to the airflow environments directory
 airflow_env_rel_path=src/ingest-pipeline/misc/tools/airflow_environments
 
-
 source /etc/os-release
 platform_string="${ID}_${VERSION_ID}_${instance}"
 platform_file="${top_level_dir}/${airflow_env_rel_path}/env_${platform_string}.sh"
