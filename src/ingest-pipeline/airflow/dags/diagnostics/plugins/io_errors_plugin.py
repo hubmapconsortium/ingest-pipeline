ffe698a5afac005f4d30d9b9766ec3521bb67d48
Gesina Phillips <gesina@psc.edu>
2022-12-01 13:27:43 -0500

    plugin for finding OSError input/output errors

---
+++ src/ingest-pipeline/airflow/dags/diagnostics/plugins/io_errors_plugin.py
@@ -0,0 +1,34 @@
+"""
+This plugin uses regex to search for IO errors in session.log
+"""
+
+import re
+from pathlib import Path
+
+from diagnostics.diagnostic_plugin import (
+    DiagnosticPlugin,
+    DiagnosticResult,
+)
+
+IO_ERROR_REGEX = r"(?=.*(OSError))(?=.*(Input\/output error))"
+
+
+class FindErrorsDiagnosticPlugin(DiagnosticPlugin):
+    description = "This test should locate instances of the string 'Error'"
+    order_of_application = 1.0
+
+    def __init__(self, **kwargs):
+        self.dir_path = Path(kwargs["local_directory_full_path"])
+
+    def diagnose(self):
+        session_log_path = self.dir_path / "session.log"
+        assert session_log_path.exists(), "session.log is not in the dataset directory"
+        regex = re.compile(IO_ERROR_REGEX)
+        errors = []
+        for line in open(session_log_path):
+            match = regex.search(line)
+            if match:
+                # TODO: Currently adds the entire line, could instead return just error name
+                # and/or do more parsing to separate error name and contextual info
+                errors.append(line)
+        return DiagnosticResult(errors)
