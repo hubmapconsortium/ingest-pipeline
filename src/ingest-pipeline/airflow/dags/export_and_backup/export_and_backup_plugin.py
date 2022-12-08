91da7e12f733032ed3689c4301cdc6d383169201
Gesina Phillips <gesina@psc.edu>
2022-12-08 15:40:30 -0500

    fixing a weirdness

--- src/ingest-pipeline/airflow/dags/export_and_backup/export_and_backup_plugin.py
+++ src/ingest-pipeline/airflow/dags/export_and_backup/export_and_backup_plugin.py
@@ -1,6 +1,14 @@
-from export_and_backup.export_and_backup_plugin import ExportAndBackupPlugin
+from pathlib import Path
 
 
-class AlwaysPassesPlugin(ExportAndBackupPlugin):
+class ExportAndBackupPlugin:
     def run_plugin(self):
-        return "I passed!"
+        raise NotImplementedError()
+
+
+def export_and_backup_result_iter(plugin_dir, **kwargs) -> list:
+    plugin_dir = Path(plugin_dir)
+    plugins = list(plugin_dir.glob("*.py"))
+    if not plugins:
+        raise Exception(f"{plugin_dir}/*.py does not match any export_and_backup plugins")
+    return plugins
--- src/ingest-pipeline/airflow/dags/export_and_backup/plugins/always_passes_plugin.py
+++ src/ingest-pipeline/airflow/dags/export_and_backup/plugins/always_passes_plugin.py
@@ -1,14 +1,6 @@
-from pathlib import Path
+from export_and_backup.export_and_backup_plugin import ExportAndBackupPlugin
 
 
-class ExportAndBackupPlugin:
+class AlwaysPassesPlugin(ExportAndBackupPlugin):
     def run_plugin(self):
-        raise NotImplementedError()
-
-
-def export_and_backup_result_iter(plugin_dir, **kwargs) -> list:
-    plugin_dir = Path(plugin_dir)
-    plugins = list(plugin_dir.glob("*.py"))
-    if not plugins:
-        raise Exception(f"{plugin_dir}/*.py does not match any export_and_backup plugins")
-    return plugins
+        return "I passed!"
