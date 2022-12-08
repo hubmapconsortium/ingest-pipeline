1b000a14b224524b6c9a8e0c4ba41cc536d85522
Gesina Phillips <gesina@psc.edu>
2022-12-08 15:54:57 -0500

    fixing export_and_backup_result_iter

--- src/ingest-pipeline/airflow/dags/export_and_backup/plugins/always_passes_plugin.py
+++ src/ingest-pipeline/airflow/dags/export_and_backup/plugins/always_passes_plugin.py
@@ -2,5 +2,7 @@ from export_and_backup.export_and_backup_plugin import ExportAndBackupPlugin
 
 
 class AlwaysPassesPlugin(ExportAndBackupPlugin):
+    description = "This plugin always passes"
+
     def run_plugin(self):
         return "I passed!"
