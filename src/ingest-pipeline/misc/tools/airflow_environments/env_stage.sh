aca99829dfb741ad046760423c561b532539e6ea
David Betancur <dbetancur@psc.edu>
2023-01-24 12:31:07 -0500

    Upgraded version of Airflow. Bugfix on Globus Auth. Added SenNet endpoints.

--- src/ingest-pipeline/misc/tools/airflow_environments/env_stage.sh
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_stage.sh
@@ -2,8 +2,8 @@
 HM_AF_CONFIG=/opt/repositories/stage/sennet/ingest-pipeline/src/ingest-pipeline/airflow/airflow.cfg
 HM_AF_HOME=/opt/repositories/stage/sennet/ingest-pipeline/src/ingest-pipeline/airflow
 
-HM_AF_CONN_INGEST_API_CONNECTION=http://hivevm191.psc.edu:7777/
-HM_AF_CONN_UUID_API_CONNECTION=http://https%3a%2f%2fuuid-api.stage.hubmapconsortium.org/
-HM_AF_CONN_CELLS_API_CONNECTION=http://https%3a%2f%2fcells-api.stage.hubmapconsortium.org/
-HM_AF_CONN_SEARCH_API_CONNECTION=http://https%3a%2f%2fsearch-api.stage.hubmapconsortium.org%2fv3/
-HM_AF_CONN_ENTITY_API_CONNECTION=http://https%3a%2f%2fentity-api.stage.hubmapconsortium.org/
+HM_AF_CONN_INGEST_API_CONNECTION=http://https%3a%2f%2fingest-api.stage.sennetconsortium.org/
+HM_AF_CONN_UUID_API_CONNECTION=http://https%3a%2f%2fuuid-api.stage.sennetconsortium.org/
+HM_AF_CONN_CELLS_API_CONNECTION=http://https%3a%2f%2fcells-api.stage.sennetconsortium.org/
+HM_AF_CONN_SEARCH_API_CONNECTION=http://https%3a%2f%2fsearch-api.stage.sennetconsortium.org%2fv3/
+HM_AF_CONN_ENTITY_API_CONNECTION=http://https%3a%2f%2fentity-api.stage.sennetconsortium.org/
