09b8895a559294ca6f3c3877b3846fc5b6cfe20c
Hubmap Hive <hive@hivevm201.psc.edu>
2023-02-16 22:31:14 -0500

    Deltas found in place in the pittdev tree

--- src/ingest-pipeline/misc/tools/airflow_environments/env_centos_7_dev.sh
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_centos_7_dev.sh
@@ -2,9 +2,9 @@
 
 # HM_AF_METHOD must be one of venv, module_conda, or conda
 # HM_AF_ENV_NAME must be the name of the conda environment or the full path to the venv dir
-# HM_AF_METHOD='module_conda'
+#HM_AF_METHOD='module_conda'
 HM_AF_METHOD='conda'
-HM_AF_ENV_NAME='condaEnv_centos_7_python_3.6_dev'
+HM_AF_ENV_NAME='condaEnv_centos_7_python_3.9_dev'
 # HM_AF_METHOD='venv'
 # HM_AF_ENV_NAME='/hive/users/hive/hubmap/hivevm191-dev/venv'
 
