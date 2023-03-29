fb705d066827bea877853c6bf805a78b21ed6911
David Betancur <dbetancur@psc.edu>
2023-03-29 10:19:42 -0400

    Updated environment for new search-api url

--- src/ingest-pipeline/misc/tools/airflow_environments/env_prod.sh
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_prod.sh
@@ -5,5 +5,5 @@ HM_AF_HOME=/opt/repositories/prod/sennet/ingest-pipeline/src/ingest-pipeline/air
 HM_AF_CONN_INGEST_API_CONNECTION=http://https%3a%2f%2fingest.api.sennetconsortium.org/
 HM_AF_CONN_UUID_API_CONNECTION=http://https%3a%2f%2fuuid.api.sennetconsortium.org/
 HM_AF_CONN_CELLS_API_CONNECTION=http://https%3a%2f%2fcells.api.sennetconsortium.org/
-HM_AF_CONN_SEARCH_API_CONNECTION=http://https%3a%2f%2fsearch.api.sennetconsortium.org%2fv3/
+HM_AF_CONN_SEARCH_API_CONNECTION=http://https%3a%2f%2fsearch.api.sennetconsortium.org/
 HM_AF_CONN_ENTITY_API_CONNECTION=http://https%3a%2f%2fentity.api.sennetconsortium.org/
