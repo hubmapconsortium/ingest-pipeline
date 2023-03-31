2a0f5de1882859e23b602e0549d9bc0137c4d9d6
David Betancur <dbetancur@psc.edu>
2023-03-31 11:42:58 -0400

    Updated environment scripts to use the new global python version variable to build the conda environments

--- src/ingest-pipeline/misc/tools/airflow_environments/env_centos_7_test.sh
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_centos_7_test.sh
@@ -3,7 +3,7 @@
 # HM_AF_METHOD must be one of venv, module_conda, or conda
 # HM_AF_ENV_NAME must be the name of the conda environment or the full path to the venv dir
 HM_AF_METHOD='conda'
-HM_AF_ENV_NAME='condaEnv_centos_7_python_3.6_test'
+HM_AF_ENV_NAME="condaEnv_centos_7_python_${HUBMAP_PYTHON_VERSION}_test"
 # HM_AF_METHOD='venv'
 # HM_AF_ENV_NAME='/hive/users/hive/hubmap/hivevm192-test/venv'
 
