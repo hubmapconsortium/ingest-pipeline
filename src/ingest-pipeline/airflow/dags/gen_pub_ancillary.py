9a2f75aba83bb07e75def2a40c74f16fcf6309a7
Hubmap Hive <hive@hivevm192.psc.edu>
2023-05-09 16:40:10 -0400

    Working gen_pub_ancillary workflow, and support files

--- src/ingest-pipeline/airflow/dags/gen_pub_ancillary.py
+++ src/ingest-pipeline/airflow/dags/gen_pub_ancillary.py
@@ -9,13 +9,17 @@ from collections import defaultdict
 import frontmatter
 
 from airflow.configuration import conf as airflow_conf
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.exceptions import AirflowException
 from airflow.providers.http.hooks.http import HttpHook
 
 from hubmap_operators.common_operators import (
     CreateTmpDirOperator,
     CleanupTmpDirOperator,
+    LogInfoOperator,
+    JoinOperator,
+    MoveDataOperator,
+    SetDatasetProcessingOperator,
 )
 
 from utils import (
@@ -23,7 +27,14 @@ from utils import (
     encrypt_tok,
     get_tmp_dir_path,
     get_auth_tok,
+    get_dataset_uuid,
+    get_previous_revision_uuid,
+    build_dataset_name as inner_build_dataset_name,
     pythonop_get_dataset_state,
+    pythonop_set_dataset_state,
+    pythonop_send_create_dataset,
+    pythonop_maybe_keep,
+    get_parent_dataset_uuids_list,
     HMDAG,
     get_queue_resource,
     get_preserve_scratch_resource,
@@ -57,6 +68,11 @@ with HMDAG(
         "preserve_scratch": get_preserve_scratch_resource("gen_pub_ancillary"),
     },
 ) as dag:
+
+    pipeline_name = 'gen_pub_ancillary'
+
+    def build_dataset_name(**kwargs):
+        return inner_build_dataset_name(dag.dag_id, pipeline_name, **kwargs)
 
     def find_uuid(**kwargs):
         parent_l = kwargs["dag_run"].conf["parent_submission_id"]
@@ -98,85 +114,101 @@ with HMDAG(
         provide_context=True,
     )
 
+    def get_pub_description(uuid, **kwargs):
+        method = 'GET'
+        auth_tok = get_auth_tok(**kwargs)
+        headers = {
+            'authorization': f'Bearer {auth_tok}',
+            'content-type': 'application/json',
+            'X-Hubmap-Application': 'ingest-pipeline',
+        }
+        http_hook = HttpHook(method, http_conn_id='entity_api_connection')
+        endpoint = f'entities/{uuid}'
+        try:
+            response = http_hook.run(endpoint,
+                                     headers=headers,
+                                     extra_options={'check_response': False})
+            response.raise_for_status()
+            ds_rslt = response.json()
+            return ds_rslt['description']
+        except HTTPError as e:
+            print(f'ERROR: {e}')
+            if e.response.status_code == codes.unauthorized:
+                raise RuntimeError('entity database authorization was rejected?')
+            else:
+                print('benign error')
+                return ""
+
+
     def build_ancillary_data(**kwargs):
-        uuid = kwargs["ti"].xcom_pull(key="uuid")
-        lz_path = kwargs["ti"].xcom_pull(key="lz_path")
-        top_path = Path(lz_path)
-        vignette_path = top_path / 'vignettes'
-        assert vignette_path.is_dir(), 'vignettes not found'
-        pub_path = top_path / 'publication.md'
-        assert pub_path.is_file(), 'publication.md not found'
-        rslt = {}
-        pub_fm = frontmatter.loads(pub_path.read_text())
-        pub_md = pub_fm.metadata
-        print('publication metadata follows')
-        pprint(pub_md)
-        pub_content = pub_fm.content
-        print('publication content follows')
-        pprint(pub_content)
-        rslt['vignettes'] = []
-        rslt['description'] = pub_content
-        assert 'name' in pub_md, 'description metadata does not include a name'
-        rslt['name'] = pub_md['name']
-        if 'figures' in pub_md:
-            fig_list = pub_md['figures']
-        else:
-            fig_list = []
-        all_figs = defaultdict(list)
-        for fig_d in fig_list:
-            assert 'name' in fig_d, 'description figure has no name'
-            assert 'file' in fig_d, f"figure {fig_d['name']} has no json block"
-            fig_json_path = pub_path.parent / fig_d['file']
-            assert fig_json_path.suffix == '.json', f'figure {fig_json_path} is not json'
-            rel_path = fig_json_path.relative_to(vignette_path)
-            vig_path = rel_path.parents[0]
-            all_figs[vig_path].append({
-                'name': fig_d['name'],
-                'file': rel_path.relative_to(vig_path)
-            })
-        print('all_figs follows')
-        pprint(all_figs)
-        
-        for this_vignette_path in vignette_path.glob('*'):
-            print(f'this_vignette_path {this_vignette_path}')
-            assert this_vignette_path.is_dir(), 'Found a non-dir in vignettes'
-            print(f'looking up {this_vignette_path.name} in {all_figs.keys()}')
-            this_vignette_figs = all_figs.get(Path(this_vignette_path.name), [])
-            print('this_vignette_figs follows')
-            pprint(this_vignette_figs)
-            this_vignette_json_names = {elt['file'].name: elt['name'] for elt in this_vignette_figs}
-            print('this_vignette_json_names follows')
-            pprint(this_vignette_json_names)
-            this_description = None
-            this_block = {
-                'directory_name': this_vignette_path.name,
-                'figures': []
+        try:
+            uuid = kwargs["ti"].xcom_pull(key="uuid")
+            lz_path = kwargs["ti"].xcom_pull(key="lz_path")
+            top_path = Path(lz_path)
+            vignette_path = top_path / 'vignettes'
+            assert vignette_path.is_dir(), 'vignettes not found'
+            rslt = {
+                'description': get_pub_description(uuid, **kwargs),
+                'vignettes': []
             }
-            for file_path in this_vignette_path.glob('*'):
-                print(f'file in this vignette: {file_path}')
-                assert file_path.is_file(), 'Found a subdir in a vignette'
-                if file_path.suffix == '.md':
-                    assert this_description is None, f"vignette has two descriptions"
-                    vig_fm = frontmatter.loads(file_path.read_text())
-                    # Any metadata is ignored
-                    print('vig_fm metadata follows')
-                    pprint(vig_fm.metadata)
-                    this_block['name'] = vig_fm.metadata['name']
-                    this_block['description'] = vig_fm.content
-                else:
-                    print(f'check this_vignette_figs for {file_path}')
-                    assert file_path.name in this_vignette_json_names, f"{file_path.name} is misplaced"
-                    this_block['figures'].append({
-                        'name': this_vignette_json_names[file_path.name],
-                        'file': file_path.name
-                    })
-                    
-            rslt['vignettes'].append(this_block)
-                
-        print('rslt follows')
-        pprint(rslt)
-        assert_json_matches_schema(rslt, 'publication_ancillary_schema.yml')
-        return rslt
+
+            for this_vignette_path in vignette_path.glob('*'):
+                #print(f'this_vignette_path {this_vignette_path}')
+                assert this_vignette_path.is_dir(), f'Found the non-dir {this_vignette_path} in vignettes'
+                vig_block = {
+                    'figures': [],
+                    'directory_name': str(this_vignette_path.name)
+                }
+                this_vignette_all_paths = set(this_vignette_path.glob('*'))
+                #print('this_vignette_all_paths follows')
+                #pprint(this_vignette_all_paths)
+                if not all(pth.is_file() for pth in this_vignette_all_paths):
+                    raise AssertionError('Found a subdirectory in a vignette')
+                md_found = False
+                for md_path in this_vignette_path.glob('*.md'):
+                    #print(f'md in this vignette: {md_path}')
+                    if md_found:
+                        raise AssertionError('A vignette has more than one markdown file')
+                    else:
+                        md_found = True
+                    vig_fm = frontmatter.loads(md_path.read_text())
+                    #print('vig_fm metadata follows')
+                    #pprint(vig_fm.metadata)
+                    #print('vig_fm content follows')
+                    #pprint(vig_fm.content)
+                    for key in ['name', 'figures']:
+                        assert key in vig_fm.metadata, f'vignette markdown has no {key}'
+                    vig_block['name'] = vig_fm.metadata['name']
+                    vig_block['description'] = vig_fm.content
+                    for fig_dict in vig_fm.metadata['figures']:
+                        assert 'file' in fig_dict, 'figure dict does not reference a file'
+                        assert 'name' in fig_dict, 'figure dict does not provide a name'
+                        vig_block['figures'].append({
+                            'name': fig_dict['name'],
+                            'file': fig_dict['file'],
+                        })
+                    this_vignette_all_paths.remove(md_path)
+                    for fig in vig_block['figures']:
+                        this_vignette_all_paths.remove(this_vignette_path / fig['file'])
+                    assert not this_vignette_all_paths, ('unexpected files in vignette:'
+                                                         f' {this_vignette_all_paths}')
+
+                rslt['vignettes'].append(vig_block)
+
+            print('rslt follows')
+            pprint(rslt)
+            assert_json_matches_schema(rslt, 'publication_ancillary_schema.yml')
+            run_id = kwargs['run_id']
+            tmpdir = get_tmp_dir_path(kwargs['run_id'])
+            tmpdir_path = Path(tmpdir)
+            (tmpdir_path / 'cwl_out').mkdir(exist_ok=True)
+            with open(f'{tmpdir}/cwl_out/publication_ancillary.json', 'w') as f:
+                json.dump(rslt, f, indent=4)
+            kwargs['ti'].xcom_push(key='return_value', value=0)  # signal success
+        except AssertionError as excp:
+            print(f'Operation failed because of: {excp}')
+            kwargs['ti'].xcom_push(key='return_value', value=1)  # signal failure
+
 
     t_build_ancillary_data = PythonOperator(
         task_id="build_ancillary_data",
@@ -225,7 +257,59 @@ with HMDAG(
         provide_context=True,
     )
 
+    # Others
+    t_send_create_dataset = PythonOperator(
+        task_id='send_create_dataset',
+        python_callable=pythonop_send_create_dataset,
+        provide_context=True,
+        op_kwargs={'parent_dataset_uuid_callable': get_parent_dataset_uuids_list,
+                   'previous_revision_uuid_callable': get_previous_revision_uuid,
+                   'http_conn_id': 'ingest_api_connection',
+                   'dataset_name_callable': build_dataset_name,
+                   'dataset_types': ["publication_ancillary"]
+                   }
+    )
+
+    t_set_dataset_error = PythonOperator(
+        task_id='set_dataset_error',
+        python_callable=pythonop_set_dataset_state,
+        provide_context=True,
+        trigger_rule='all_done',
+        op_kwargs={'dataset_uuid_callable': get_dataset_uuid,
+                   'ds_state': 'Error',
+                   'message': 'An error occurred in {}'.format(pipeline_name)
+                   }
+    )
+
+    t_maybe_keep_ancillary_data = BranchPythonOperator(
+        task_id='maybe_keep_ancillary_data',
+        python_callable=pythonop_maybe_keep,
+        provide_context=True,
+        op_kwargs={
+            'next_op': 'move_data',
+            'bail_op': 'set_dataset_error',
+            'test_op': 'build_ancillary_data',
+        }
+    )
+
+    t_log_info = LogInfoOperator(task_id='log_info')
+    t_join = JoinOperator(task_id='join')
+    t_set_dataset_processing = SetDatasetProcessingOperator(task_id='set_dataset_processing')
+    t_move_data = MoveDataOperator(task_id='move_data')
     t_create_tmpdir = CreateTmpDirOperator(task_id="create_temp_dir")
     t_cleanup_tmpdir = CleanupTmpDirOperator(task_id="cleanup_temp_dir")
 
-    t_create_tmpdir >> t_find_uuid >> t_build_ancillary_data >> t_send_status >> t_cleanup_tmpdir
+    # DAG
+    (
+        t_log_info >> t_create_tmpdir 
+        >> t_send_create_dataset >> t_set_dataset_processing
+        >> t_find_uuid 
+        >> t_build_ancillary_data
+        >> t_maybe_keep_ancillary_data
+        >> t_move_data
+        >> t_send_status 
+        >> t_join
+    )
+    t_maybe_keep_ancillary_data >> t_set_dataset_error
+    t_set_dataset_error >> t_join
+    t_join >> t_cleanup_tmpdir
