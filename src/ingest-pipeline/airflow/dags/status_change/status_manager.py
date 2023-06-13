8b96c9234f9be82d165d0adcef4b1d641abcafb3
Gesina Phillips <gesina@psc.edu>
2023-06-13 16:55:40 -0400

    unfinished code to create Asana cards for child datasets of uploads; potentially breaking changes in split_and_create

--- src/ingest-pipeline/airflow/dags/status_change/status_manager.py
+++ src/ingest-pipeline/airflow/dags/status_change/status_manager.py
@@ -12,15 +12,17 @@ from airflow.providers.http.hooks.http import HttpHook
 
 """
 TODO:
+    - Add dag to run nightly to check statuses
     - Emails?
     - Determine what extra_fields (e.g. validation_message for invalid status(es)) are required for each status and how to manage them
 """
 
 
 class Statuses(Enum):
-    DATASET_ABANDONED = "Abandoned"
-    DATASET_APPROVED = "Approved"
+    # Dataset Hold and Deprecated are not currently in use but are valid for Entity API
+    DATASET_DEPRECATED = "Deprecated"
     DATASET_ERROR = "Error"
+    DATASET_HOLD = "Hold"
     DATASET_INVALID = "Invalid"
     DATASET_NEW = "New"
     DATASET_PROCESSING = "Processing"
@@ -56,7 +58,8 @@ Example usage, default path (update status, update Asana):
             {
                 "extra_fields": {},
                 "extra_options": {},
-            }
+            },
+            #optional http_conn_id="entity_api_connection"
         ).on_status_change()
 """
 
@@ -69,12 +72,14 @@ class StatusChanger:
         status: Statuses,
         extras: StatusChangerExtras,
         http_conn_id: str = "entity_api_connection",
+        verbose: bool = True,
     ):
         self.uuid = uuid
         self.token = token
         self.status = status
         self.extras = extras
         self.http_conn_id = http_conn_id
+        self.verbose = verbose
 
     def format_status_data(self) -> dict:
         if not type(self.status) == Statuses:
@@ -106,16 +111,18 @@ class StatusChanger:
             """
         )
         try:
-            logging.info(f"Setting status to {data['status']}...")
+            if self.verbose:
+                logging.info(f"Setting status to {data['status']}...")
             extra_options = self.extras["extra_options"].update({"check_response": True})
             response = http_hook.run(endpoint, json.dumps(data), headers, extra_options)
-            logging.info(
-                f"""
-                    Status set to {response.json()['status']}.
-                    Response:
-                    {response}
-                """
-            )
+            if self.verbose:
+                logging.info(
+                    f"""
+                        Status set to {response.json()['status']}.
+                        Response:
+                        {response}
+                    """
+                )
         except Exception as e:
             raise StatusChangerException(
                 f"Encountered error with request to change status for {self.uuid}, status not set. Error: {e}"
@@ -160,11 +167,11 @@ class UpdateAsana:
         self.submission_data = get_submission_context(token, uuid)
         self.custom_field_gids()
 
+        # If any Process Stage enum values change in Asana, update them here
         self.asana_status_map = {
-            Statuses.DATASET_ABANDONED: self.process_stage_gids["Blocked 🛑"],
-            Statuses.DATASET_APPROVED: self.process_stage_gids["Publishing"],
             Statuses.DATASET_ERROR: self.process_stage_gids["Blocked 🛑"],
             Statuses.DATASET_INVALID: self.process_stage_gids["Blocked 🛑"],
+            Statuses.DATASET_NEW: self.process_stage_gids["Intake"],
             Statuses.DATASET_PUBLISHED: self.process_stage_gids["Publishing"],
             Statuses.DATASET_QA: self.process_stage_gids["Post-Processing"],
             Statuses.UPLOAD_ERROR: self.process_stage_gids["Blocked 🛑"],
@@ -174,66 +181,6 @@ class UpdateAsana:
             Statuses.UPLOAD_SUBMITTED: self.process_stage_gids["Pre-Processing"],
             Statuses.UPLOAD_VALID: self.process_stage_gids["Pre-Processing"],
         }
-
-    # @cached_property
-    # def get_sibling_datasets(self) -> list:
-    #     # @note need to check on how derived datasets factor in
-    #     """
-    #     Should only be called for entity_type "Dataset".
-    #     Check first whether the Asana task is labeled "Datasets".
-    #     If so, retrieve other datasets in the "HuBMAP ID" field.
-    #     If not, first retrieve parent upload from Entity API and
-    #     then query its child datasets.
-    #     """
-    #     sibling_datasets = []
-    #     status = ""
-    #     task = self.client.tasks.get_task(self.hubmap_id_field)
-    #     for field in task["custom_fields"]:
-    #         # Using Status/In progress as a stand-in for the entity type field
-    #         if field["name"] == "Status":
-    #             status = field["enum_value"]
-    #     # if status == "Dataset":
-    #     if status == "In progress":
-    #         for field in task["custom_fields"]:
-    #             if field["name"] == "HuBMAP ID":
-    #                 sibling_datasets = field["text_value"].split(" ")
-    #                 return sibling_datasets
-    #     else:
-    #         try:
-    #             parent_upload_uuid = get_submission_context(
-    #                 self.token, self.submission_data["upload"]["uuid"]
-    #             ).get("uuid")
-    #             if parent_upload_uuid:
-    #                 sibling_dataset_info = get_submission_context(self.token, parent_upload_uuid)
-    #                 for dataset in sibling_dataset_info:
-    #                     sibling_datasets.append(dataset.get("hubmap_id"))
-    #             return sibling_datasets
-    #         except Exception as e:
-    #             raise StatusChangerException(
-    #                 f"Retrieving sibling datasets from Entity API for {self.hubmap_id} failed! Error: {e}"
-    #             )
-    #     return []
-    #
-    # def progress_task(self) -> bool:
-    #     """
-    #     Check dataset statuses first to see if any are in a status that
-    #     would block the Asana task from being updated. If not, check whether
-    #     the status to be changed to is Approved/Published. In that case, make
-    #     sure all sibling datasets are also through QA.
-    #     """
-    #     sibling_datasets = self.get_sibling_datasets
-    #     for dataset in sibling_datasets:
-    #         status = get_submission_context(self.token, dataset).get("status")
-    #         if status in [
-    #             Statuses.DATASET_PROCESSING.value,
-    #             Statuses.DATASET_ERROR.value,
-    #             Statuses.DATASET_INVALID.value,
-    #             Statuses.DATASET_NEW.value,
-    #         ]:
-    #             return False
-    #         elif self.status in [Statuses.DATASET_APPROVED, Statuses.DATASET_PUBLISHED]:
-    #             if status == Statuses.DATASET_QA:
-    #                 return False
 
     def custom_field_gids(self) -> None:
         response = self.client.custom_field_settings.get_custom_field_settings_for_project(
@@ -245,12 +192,18 @@ class UpdateAsana:
                 name = custom_field["custom_field"]["name"]
             except Exception:
                 continue
+            # possible to use data dict here instead?
             if name == "HuBMAP ID":
                 self.hubmap_id_gid = custom_field["custom_field"]["gid"]
             elif name == "Process Stage":
                 self.process_stage_field_gid = custom_field["custom_field"]["gid"]
                 for stage in custom_field["custom_field"]["enum_options"]:
                     self.process_stage_gids[stage["name"]] = stage["gid"]
+            # check naming of these fields in Asana
+            elif name == "Parent":
+                self.parent_field_gid = custom_field["custom_field"]["gid"]
+            elif name == "Entity Type":
+                self.entity_type_field_gid = custom_field["custom_field"]["gid"]
         if (
             not self.process_stage_gids
             or not self.hubmap_id_gid
@@ -262,7 +215,7 @@ class UpdateAsana:
                             Process Stage GIDs: {self.process_stage_gids}
                             Process Stage field GID: {self.process_stage_field_gid}
                             HuBMAP ID field GID: {self.hubmap_id_gid}
-                            """
+                """
             )
 
     @cached_property
@@ -271,6 +224,7 @@ class UpdateAsana:
         Given a HuBMAP ID, find the associated Asana task.
         Fails if a HuBMAP ID is associated with 0 or >1 tasks.
         """
+        # TODO: this might need to change for grouped dataset tasks--HuBMAP ID would be present in two cards
         response = self.client.tasks.search_tasks_for_workspace(
             self.workspace,
             {
@@ -316,35 +270,12 @@ class UpdateAsana:
         return asana_status
 
     def update_process_stage(self) -> None:
-        # if (self.submission_data["entity_type"] == "Dataset") and (self.progress_task() is False):
-        #     try:
-        #         notes_field = self.client.tasks.get_task(self.get_task_by_hubmap_id).get("notes")
-        #         response = self.client.tasks.update_task(
-        #             self.get_task_by_hubmap_id,
-        #             {
-        #                 "notes": notes_field
-        #                 + f"{self.hubmap_id} set to {self.status.value} on {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"
-        #             },
-        #         )
-        #         logging.info(
-        #             f"""Notes for task {self.get_task_by_hubmap_id} updated. Process Stage for task not updated because all datasets are not yet in appropriate statuses.
-        #             Response: {response}
-        #             """
-        #         )
-        #         return
-        #     except Exception as e:
-        #         raise StatusChangerException(
-        #             f"""Error updating notes for task {self.get_task_by_hubmap_id} occurred while checking sibling dataset statuses for {self.hubmap_id}.
-        #                         Error: {e}
-        #                         """
-        #         )
-        # logging.info(
-        #     f"Dataset {self.hubmap_id} and all sibling datasets are in state {self.status} or are further along in process; updating Process Stage for task {self.get_task_by_hubmap_id} to {self.get_asana_status}."
-        # )
+        if self.status == Statuses.UPLOAD_REORGANIZED:
+            self.create_dataset_cards()
         try:
             response = self.client.tasks.update_task(
                 self.get_task_by_hubmap_id,
-                {"custom_fields": {self.process_stage_gid: self.get_asana_status}},
+                {"custom_fields": {self.process_stage_field_gid: self.get_asana_status}},
                 opt_pretty=True,
             )
             logging.info(f"UPDATE SUCCESSFUL: {response}")
@@ -352,3 +283,31 @@ class UpdateAsana:
             raise StatusChangerException(
                 f"Error occurred while updating HuBMAP ID {self.hubmap_id}; not updated. Error: {e}"
             )
+
+    def create_dataset_cards(self):
+        # TODO: assign dataset hubmap ids to list and count
+        logging.info(f"Upload {self.hubmap_id} has n child datasets. Creating Asana cards...")
+        try:
+            # for dataset_id in child_datasets:
+            # get fields for title (should be in self.submission_data)
+            response = self.client.tasks.create_task(
+                {
+                    "custom_fields": {
+                        # "title": "Data Provider institution | assay type | submission date"
+                        # self.hubmap_id_gid: dataset_id,
+                        # TODO: is "New" correct? Should it be mapped to Intake?
+                        self.process_stage_field_gid: "New",
+                        self.entity_type_field_gid: "Dataset",
+                        self.parent_field_gid: self.hubmap_id,
+                        # TODO: does parent_field_gid need to be extensible for card IDs for non-upload groups? is that a separate field or is this actually a parent milestone ID field?
+                    },
+                    "projects": [self.project],
+                },
+                opt_pretty=True,
+            )
+            # logging.info(f"Card created successfully for dataset {dataset_id}")
+        except Exception:
+            raise StatusChangerException(
+                # f"Error creating dataset card for dataset {hubmap_id}, part of reorganized dataset {self.hubmap_id}"
+            )
+        logging.info(f"All dataset cards created for upload {self.hubmap_id}")
