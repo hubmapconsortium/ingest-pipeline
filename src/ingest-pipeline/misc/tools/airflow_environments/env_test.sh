3e0ee197c1502c309cb5960279f04be4c2270e50
jpuerto-psc <68066250+jpuerto-psc@users.noreply.github.com>
2023-06-26 11:26:49 -0400

    Update env_test.sh

--- src/ingest-pipeline/misc/tools/airflow_environments/env_test.sh
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_test.sh
@@ -1,8 +1,8 @@
 #set airflow environment
 HM_AF_CONFIG=/opt/repositories/vm002-test/ingest-pipeline/src/ingest-pipeline/airflow/airflow.cfg
 HM_AF_HOME=/opt/repositories/vm002-test/ingest-pipeline/src/ingest-pipeline/airflow
-HM_AF_CONN_INGEST_API_CONNECTION=http://vm002.hive.psc.edu:7777/
 
+HM_AF_CONN_INGEST_API_CONNECTION=http://vm002.hive.psc.edu:7777/
 HM_AF_CONN_UUID_API_CONNECTION=http://https%3a%2f%2fuuid-api.test.hubmapconsortium.org/
 HM_AF_CONN_FILES_API_CONNECTION=http://https%3a%2f%2ffiles-api.test.hubmapconsortium.org/
 HM_AF_CONN_SPATIAL_API_CONNECTION=http://https%3a%2f%2fspatial-api.test.hubmapconsortium.org/
