681d4a2dc203c8f765a7519695a012f2b9894bd9
David Betancur <dbetancur@psc.edu>
2023-08-25 13:09:22 -0400

    Bugfix paths and environment variables for new infrastructure.

--- src/ingest-pipeline/misc/tools/airflow_environments/env_test.sh
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_test.sh
@@ -1,8 +1,8 @@
 #set airflow environment
-HM_AF_CONFIG=/hive/users/hive/hubmap/hivevm192-test/ingest-pipeline/src/ingest-pipeline/airflow/airflow.cfg
-HM_AF_HOME=/hive/users/hive/hubmap/hivevm192-test/ingest-pipeline/src/ingest-pipeline/airflow
+HM_AF_CONFIG=/opt/repositories/vm002-test/ingest-pipeline/src/ingest-pipeline/airflow/airflow.cfg
+HM_AF_HOME=/opt/repositories/vm002-test/ingest-pipeline/src/ingest-pipeline/airflow
 
-HM_AF_CONN_INGEST_API_CONNECTION=http://hivevm192.psc.edu:7777/
+HM_AF_CONN_INGEST_API_CONNECTION=http://vm002.hive.psc.edu:7777/
 HM_AF_CONN_UUID_API_CONNECTION=http://https%3a%2f%2fuuid-api.test.hubmapconsortium.org/
 HM_AF_CONN_FILES_API_CONNECTION=http://https%3a%2f%2ffiles-api.test.hubmapconsortium.org/
 HM_AF_CONN_SPATIAL_API_CONNECTION=http://https%3a%2f%2fspatial-api.test.hubmapconsortium.org/
