377fd534de7108be1faf09c00ccc096ea1389e67
Juan Puerto <=>
2023-09-11 11:29:26 -0400

    Submodules: Remove non-existent import

--- src/ingest-pipeline/airflow/dags/generate_bdbag.py
+++ src/ingest-pipeline/airflow/dags/generate_bdbag.py
@@ -53,7 +53,6 @@ from submodules import (
     hubmapbags_id_namespace as id_namespace,
     hubmapbags_biosample_in_collection as biosample_in_collection,
     hubmapbags_file_in_collection as file_in_collection,
-    hubmapbags_primary_dcc_contact as primary_dcc_contact,
     hubmapbags_biosample as biosample,
     hubmapbags_projects as projects,
     hubmapbags_collection as collection,
@@ -304,9 +303,6 @@ with HMDAG('generate_bdbag',
 
             print('Making file_describes_collection.tsv')
             file_describes_collection.create_manifest(hubmap_id, data_directory, output_directory)
-
-            print('Making dcc.tsv')
-            primary_dcc_contact.create_manifest(output_directory)
 
             print('Making id_namespace.tsv')
             id_namespace.create_manifest(output_directory)
