a1cdc7087c320baefb50944fd388dcfa41dbc00c
David Betancur <dbetancur@psc.edu>
2023-09-19 10:47:43 -0400

    Bugfix getting UUID for instance tagging from correct config for the particular DAG

--- src/ingest-pipeline/airflow/dags/reorganize_upload.py
+++ src/ingest-pipeline/airflow/dags/reorganize_upload.py
@@ -80,7 +80,7 @@ with HMDAG('reorganize_upload',
                'preserve_scratch': get_preserve_scratch_resource('reorganize_upload'),
            }) as dag:
     def start_new_environment(**kwargs):
-        uuid = kwargs['dag_run'].conf['submission_id']
+        uuid = kwargs['dag_run'].conf['uuid']
         instance_id = create_instance(uuid, f'Airflow {get_environment_instance()} Worker',
                                       get_instance_type(kwargs['dag_run'].conf['dag_id']))
         if instance_id is None:
@@ -258,7 +258,7 @@ with HMDAG('reorganize_upload',
         if instance_id is None:
             return 1
         else:
-            uuid = kwargs['dag_run'].conf['submission_id']
+            uuid = kwargs['dag_run'].conf['uuid']
             terminate_instance(instance_id, uuid)
         return 0
 
