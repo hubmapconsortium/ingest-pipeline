321928aed46f61f883047573b04a34d2a4d33c7c
David Betancur <dbetancur@psc.edu>
2023-10-09 10:58:03 -0400

    updating regenerate_conda_env.sh for the SLURM model (stage)

--- src/ingest-pipeline/misc/tools/airflow_environments/env_prod.sh
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_prod.sh
@@ -2,6 +2,9 @@
 HM_AF_CONFIG=/opt/repositories/vm004-prod/ingest-pipeline/src/ingest-pipeline/airflow/airflow.cfg
 HM_AF_HOME=/opt/repositories/vm004-prod/ingest-pipeline/src/ingest-pipeline/airflow
 
+HM_AF_METHOD='conda'
+HM_AF_ENV_NAME="condaEnv_python_${HUBMAP_PYTHON_VERSION}_prod"
+
 HM_AF_CONN_INGEST_API_CONNECTION=http://vm004.hive.psc.edu:7777/
 HM_AF_CONN_UUID_API_CONNECTION=http://https%3a%2f%2fuuid.api.hubmapconsortium.org/
 HM_AF_CONN_FILES_API_CONNECTION=http://https%3a%2f%2ffiles.api.hubmapconsortium.org/
