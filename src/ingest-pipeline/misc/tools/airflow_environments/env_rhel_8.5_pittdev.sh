274584ae221a82ac476715e57bcd5d4f5e532e60
David Betancur <dbetancur@psc.edu>
2023-10-09 11:36:36 -0400

    Removing unused OS related scripts Updated env variables

--- src/ingest-pipeline/misc/tools/airflow_environments/env_rhel_8.5_pittdev.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# HM_AF_METHOD must be one of venv, module_conda, or conda
-# HM_AF_ENV_NAME must be the name of the conda environment or the full path to the venv dir
-HM_AF_METHOD='conda'
-HM_AF_ENV_NAME="condaEnv_rhel_8.5_python_${HUBMAP_PYTHON_VERSION}_pittdev"
-
-PARENTDIR="$(dirname "$(readlink -f "$0")")"
-. "${PARENTDIR}/airflow_environments/env_pittdev.sh"
