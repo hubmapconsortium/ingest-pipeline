6bef95160f7bf79ef4861fcff388cc87b508ab39
Juan Puerto <=>
2023-12-13 15:48:24 -0500

    General: Show where python is being run from

--- src/ingest-pipeline/airflow/dags/scan_and_begin_processing.py
+++ src/ingest-pipeline/airflow/dags/scan_and_begin_processing.py
@@ -194,6 +194,7 @@ with HMDAG(
         top_dir="{{dag_run.conf.src_path}}" ; \
         work_dir="{{tmp_dir_path(run_id)}}" ; \
         cd $work_dir ; \
+        which python ; \
         env PYTHONPATH=${PYTHONPATH}:$top_dir \
         python $src_dir/metadata_extract.py --out ./rslt.yml --yaml "$lz_dir" \
           >> session.log 2> error.log ; \
