05861de03f6c8b4c63e0540e63806ede13291b58
David Betancur <dbetancur@psc.edu>
2024-01-18 12:48:59 -0500

    Syncing back from devel Bugfixing parameter mismatch Adding return values to control later step Adding back auth_token for GET queries

--- src/ingest-pipeline/airflow/dags/utils.py
+++ src/ingest-pipeline/airflow/dags/utils.py
@@ -1701,6 +1701,7 @@ def find_matching_endpoint(host_url: str) -> str:
     assert len(candidates) == 1, f"Found {candidates}, expected 1 match"
     return candidates[0]
 
+
 def get_soft_data(dataset_uuid, **kwargs) -> dict:
     """
     Gets the soft data type for a specific uuid.
@@ -1708,6 +1709,7 @@ def get_soft_data(dataset_uuid, **kwargs) -> dict:
     endpoint = f"/assaytype/{dataset_uuid}"
     http_hook = HttpHook("GET", http_conn_id="ingest_api_connection")
     headers = {
+        "authorization": "Bearer " + get_auth_tok(**kwargs),
         "content-type": "application/json",
         "X-Hubmap-Application": "ingest-pipeline",
     }
@@ -1726,10 +1728,12 @@ def get_soft_data(dataset_uuid, **kwargs) -> dict:
             return None
     return response
 
+
 def get_soft_data_assaytype(dataset_uuid, **kwargs) -> str:
     soft_data = get_soft_data(dataset_uuid, **kwargs)
     assert "assaytype" in soft_data, f"Could not find matching assaytype for {dataset_uuid}"
     return soft_data["assaytype"]
+
 
 def main():
     """
