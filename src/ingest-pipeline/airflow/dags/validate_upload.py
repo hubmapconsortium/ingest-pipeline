c430d7d51065826e4f6f98c27249012a9ece6936
David Betancur <dbetancur@psc.edu>
2024-01-25 12:49:07 -0500

    Adding app_context parameter to IVT for correct environment testing.

--- src/ingest-pipeline/airflow/dags/validate_upload.py
+++ src/ingest-pipeline/airflow/dags/validate_upload.py
@@ -32,6 +32,8 @@ from aws_utils import (
 from airflow.configuration import conf as airflow_conf
 from airflow.exceptions import AirflowException
 from airflow.operators.python import PythonOperator
+from airflow.providers.http.hooks.http import HttpHook
+
 
 sys.path.append(airflow_conf.as_dict()["connections"]["SRC_PATH"].strip("'").strip('"'))
 
@@ -127,6 +129,10 @@ with HMDAG(
         plugin_path = [path for path in ingest_validation_tests.__path__][0]
 
         ignore_globs = [uuid, "extras", "*metadata.tsv", "validation_report.txt"]
+        app_context = {
+                'entities_url': HttpHook.get_connection("entity_api_connection").host + "/entities/",
+                'request_header': {'X-SenNet-Application': 'ingest-pipeline'}
+            }
         #
         # Uncomment offline=True below to avoid validating orcid_id URLs &etc
         #
@@ -139,6 +145,7 @@ with HMDAG(
             add_notes=False,
             extra_parameters={"coreuse": get_threads_resource("validate_upload", "run_validation")},
             globus_token=get_auth_tok(**kwargs),
+            app_context=app_context,
         )
         # Scan reports an error result
         report = ingest_validation_tools_error_report.ErrorReport(
