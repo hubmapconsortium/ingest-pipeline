f9a8adaad8099f34837bcb3cda4535459ae422aa
Juan Puerto <=>
2024-02-06 10:45:08 -0500

    General: Fix issues

--- src/ingest-pipeline/misc/tools/airflow_environments/env_dev.sh
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_dev.sh
@@ -7,5 +7,5 @@ HM_AF_CONN_UUID_API_CONNECTION=http://https%3a%2f%2fuuid-api.dev.hubmapconsortiu
 HM_AF_CONN_FILES_API_CONNECTION=http://https%3a%2f%2ffiles-api.dev.hubmapconsortium.org/
 HM_AF_CONN_SPATIAL_API_CONNECTION=http://https%3a%2f%2fspatial-api.dev.hubmapconsortium.org/
 HM_AF_CONN_CELLS_API_CONNECTION=http://https%3a%2f%2fcells-api.dev.hubmapconsortium.org/
-HM_AF_CONN_SEARCH_API_CONNECTION=http://https%3a%2f%2fontoly-api.dev.hubmapconsortium.org/
+HM_AF_CONN_SEARCH_API_CONNECTION=http://https%3a%2f%2fontology-api.dev.hubmapconsortium.org/
 HM_AF_CONN_ENTITY_API_CONNECTION=http://https%3a%2f%2fentity-api.dev.hubmapconsortium.org/
