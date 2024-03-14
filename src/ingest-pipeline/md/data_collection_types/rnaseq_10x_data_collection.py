2fdb359116003c60100a7af17a1a9a2691bf2187
David Betancur <dbetancur@psc.edu>
2024-03-14 14:50:57 -0400

    Adding missing resource_map for new visium pipeline. Adding missing workflow_map for Histology (h-and-e). Syncing bugfixes found in SenNet.

--- src/ingest-pipeline/md/data_collection_types/rnaseq_10x_data_collection.py
+++ src/ingest-pipeline/md/data_collection_types/rnaseq_10x_data_collection.py
@@ -79,7 +79,7 @@ class RNASEQ10XDataCollection(DataCollection):
         self.offsetdir = self.find_top(self.topdir, self.top_target, self.dir_regex)
         assert self.offsetdir is not None, "Wrong dataset type?"
 
-    def collect_metadata(self):
+    def collect_metadata(self, component=None, component_process=None):
         md_type_tbl = self.get_md_type_tbl()
         rslt = {}
         cl = []
