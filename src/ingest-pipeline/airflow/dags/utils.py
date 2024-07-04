64be8968d0744197181b70d49a7c2454ba995dcd
Joel Welling <welling@psc.edu>
2024-07-04 16:10:29 -0400

    Add sources and species to pythonon_get_dataset_state

--- src/ingest-pipeline/airflow/dags/utils.py
+++ src/ingest-pipeline/airflow/dags/utils.py
@@ -1018,6 +1018,19 @@ def pythonop_get_dataset_state(**kwargs) -> Dict:
     }
 
     if ds_rslt["entity_type"] == "Dataset":
+        unique_source_types = set()
+        if sources := ds_rslt.get("sources"):
+            rslt["sources"] = sources
+            for source in sources:
+                unique_source_types.add(source.get("source_type", "Human").lower())
+        if not unique_source_types:
+            species = "human"
+        elif len(unique_source_types) == 1:
+            species = unique_source_types.pop()
+        else:
+            species = "mixed"
+        rslt["species"] = species
+
         http_hook = HttpHook("GET", http_conn_id="entity_api_connection")
         endpoint = f"datasets/{ds_rslt['uuid']}/organs"
         try:
