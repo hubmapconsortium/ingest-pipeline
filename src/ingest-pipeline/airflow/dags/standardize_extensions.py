1ab2d75fa6ee6de3c93ffa7d3a983f986b0ce231
Gesina Phillips <gesina@psc.edu>
2024-07-11 14:11:55 -0400

    fixed casing issue, added example configs

--- src/ingest-pipeline/airflow/dags/standardize_extensions.py
+++ src/ingest-pipeline/airflow/dags/standardize_extensions.py
@@ -29,6 +29,23 @@ default_args = {
     'queue': get_queue_resource('validation_test'),
 }
 
+"""
+Examples:
+    - Bare-bones config that will change ".tif" extensions
+      to ".tiff" for a single upload:
+        {"uuid_list": [<uuid>]}
+    - Config that will do the same for multiple uploads:
+        {"uuid_list": [<uuid1>, <uuid2>]}
+    - Config with custom extension_pair, find but not replace:
+        {
+            "uuid_list": [<uuid1>],
+            "extension_pair": {
+                "target": ".jpg",
+                "replacement": ".jpeg"
+            }
+            "find_only": true
+        }
+"""
 with HMDAG('standardize_extensions',
            schedule_interval=None,
            is_paused_upon_creation=False,
@@ -113,19 +130,19 @@ with HMDAG('standardize_extensions',
         Can also pass {"find_only": True} to find and list but not replace
         instances of the "target" value; default is to find and replace.
         """
-        extension_pair = kwargs["dag_run"].conf.get("extension_pair", {"target": "tif", "replacement": "tiff"})
+        extension_pair = kwargs["dag_run"].conf.get("extension_pair", {"target": ".tif", "replacement": ".tiff"})
         for extension_action, ext in extension_pair.items():
             if not ext.startswith("."):
                 extension_pair[extension_action] = f".{ext}"
-        target = extension_pair.get("target")
-        replacement = extension_pair.get("replacement")
+        target = extension_pair.get("target", "").lower()
+        replacement = extension_pair.get("replacement", "").lower()
         assert target and replacement, f"Missing either target or replacement in extension_pair kwargs. Value passed in: {extension_pair}"
         # Crawl through dir for a given uuid and locate all instances of target
         target_filepaths = []
         directories = kwargs['ti'].xcom_pull(task_ids="check_directories")
         for root_path in directories:
             for dirpath, _, filenames in os.walk(root_path):
-                target_filepaths.extend([os.path.join(dirpath, file) for file in filenames if file.endswith(target)])
+                target_filepaths.extend([os.path.join(dirpath, file) for file in filenames if file.lower().endswith(target)])
         logging.info(f"Files matching extension {target}:")
         logging.info(target_filepaths)
         kwargs["ti"].xcom_push(key="target", value=target)
@@ -146,7 +163,7 @@ with HMDAG('standardize_extensions',
         replacement = kwargs['ti'].xcom_pull(task_ids="find_target_files", key="replacement")
         for file in target_filepaths:
             filename, ext = os.path.splitext(file)
-            assert ext == target, f"File path {file} is not the correct target extension, something went wrong in find_target_files step; exiting without further changes."
+            assert ext.lower() == target, f"File path {file} is not the correct target extension, something went wrong in find_target_files step; exiting without further changes."
             if find_only:
                 logging.info(f"Would have changed {file} to {filename + replacement}")
                 continue
