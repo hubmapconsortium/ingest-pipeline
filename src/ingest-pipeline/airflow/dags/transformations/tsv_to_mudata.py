46e46b145da521906eac07932b2c9741b6ae150f
Juan Puerto <=>
2024-10-03 09:05:14 -0400

    DAG: Run dataset through transformation pipeline. Includes logic for detecting whether a dataset has been transformed already or not.

--- src/ingest-pipeline/airflow/dags/transformations/tsv_to_mudata.py
+++ src/ingest-pipeline/airflow/dags/transformations/tsv_to_mudata.py
@@ -11,10 +11,7 @@ from utils import (
     get_cwltool_base_cmd,
     get_parent_dataset_uuid,
     get_named_absolute_workflows,
-    get_parent_dataset_uuids_list,
-    get_parent_data_dir,
     build_dataset_name as inner_build_dataset_name,
-    get_previous_revision_uuid,
     get_uuid_for_error,
     join_quote_command_str,
     make_send_status_msg_function,
@@ -23,7 +20,9 @@ from utils import (
     get_queue_resource,
     get_preserve_scratch_resource,
     pythonop_get_dataset_state,
-    get_dataset_type_organ_based,
+    get_dataset_uuid,
+    get_parent_dataset_path,
+    pythonop_send_create_dataset,
 )
 from hubmap_operators.common_operators import (
     CleanupTmpDirOperator,
@@ -56,7 +55,6 @@ with HMDAG(
     is_paused_upon_creation=False,
     default_args=default_args,
     user_defined_macros={
-        "parent_dir_path": get_parent_data_dir,
         "tmp_dir_path": get_tmp_dir_path,
         # TODO: Update this to use a different resource.
         "preserve_scratch": get_preserve_scratch_resource("pas_ftu_segmentation"),
@@ -64,29 +62,92 @@ with HMDAG(
 ) as dag:
     pipeline_name = "epic-obj-csv-to-mudata"
 
-    # TODO: Determine whether we need to create a new version or use the passed one
-    # Should be able to check whether or not the parent dataset has a transformations directory
-    # If it does, then we should create a new version.
-    # Once we implement a DAG of DAGs (seg mask -> csv to tsv -> tsv to mudata -> image pyramid)
-    # we can move this check into the seg mask DAG and pass it down to the others
-
-    # TODO: Create the new version and set the dataset_uuid equal to that (or to the passed id)
-
     cwl_workflows = get_named_absolute_workflows(
         mudata=Path(pipeline_name, "pipeline.cwl"),
     )
 
     t_log_info = LogInfoOperator(task_id="log_info")
-    t_create_tmpdir = CreateTmpDirOperator(task_id="create_tmpdir")
+
+    def epic_get_parent_dataset_uuids(**kwargs):
+        return pythonop_get_dataset_state(
+            dataset_uuid_callable=get_parent_dataset_uuid, **kwargs
+        ).get("parent_dataset_uuid_list", [])
+
+    def epic_get_uuid_for_revision(**kwargs):
+        status = pythonop_get_dataset_state(
+            dataset_uuid_callable=get_parent_dataset_uuid, **kwargs
+        ).get("status")
+
+        # Equivalent to grabbing the submission_id
+        return epic_get_original_dataset_uuid(**kwargs) if status.lower() == "published" else None
+
+    def epic_get_original_dataset_uuid(**kwargs):
+        return get_parent_dataset_uuid(**kwargs)
+
+    def epic_get_dataset_uuid_to_process(**kwargs):
+        # Derived dataset uuid will either be original dataset_uuid
+        # or the dataset uuid from creating a new dataset
+        return kwargs["ti"].xcom_pull(key="derived_dataset_uuid")
+
+    def epic_get_dataset_type(**kwargs):
+        return "Segmentation Mask"
 
     def build_dataset_name(**kwargs):
         return inner_build_dataset_name(dag.dag_id, pipeline_name, **kwargs)
+
+    def create_or_use_dataset(**kwargs):
+        # Check for transformation existence
+        current_dataset_path = str(get_parent_dataset_path(**kwargs))
+        kwargs["ti"].xcom_push(key="original_epic_dataset_path", value=current_dataset_path)
+        if (Path(current_dataset_path) / "extras" / "transformations").exists():
+            # Need to get the current dataset status to see whether we need to set the epic_get_revision_uuid
+            abs_path = pythonop_send_create_dataset(
+                parent_dataset_uuid_callable=epic_get_parent_dataset_uuids,
+                previous_revision_uuid_callable=epic_get_uuid_for_revision,
+                http_conn_id="ingest_api_connection",
+                dataset_name_callable=build_dataset_name,
+                dataset_type_callable=epic_get_dataset_type,
+                **kwargs
+            )
+            return abs_path
+        else:
+            kwargs["ti"].xcom_push(
+                key="derived_dataset_uuid", value=epic_get_original_dataset_uuid
+            )
+            return current_dataset_path
+
+    t_create_or_use_dataset = PythonOperator(
+        task_id="create_or_use_dataset",
+        python_callable=create_or_use_dataset,
+        provide_context=True,
+    )
+
+    # Copy data over to new dataset (excluding extras/transformations directory)
+    # If we're using the existing dataset, then it's just a copy into itself.
+    # We set $src_dir/ for the copy b/c otherwise it'll try to copy the directory
+    # as a subdirectory in the $dest_dir
+    t_copy_data = BashOperator(
+        task_id="copy_data",
+        bash_command="""
+            tmp_dir="{{tmp_dir_path(run_id)}}" ; \
+            src_dir="{{ti.xcom_pull(task_ids="create_or_use_dataset", key="original_epic_dataset_path")}}" ; \
+            dest_dir="{{ti.xcom_pull(task_ids="create_or_use_dataset")}}" ; \
+            echo "$dest_dir" ; \
+            popd ; \
+            rsync --exclude "extras/transformations/" -r "$src_dir/" "$dest_dir" >> "$tmp_dir/session.log" 2>&1 ; \
+            echo $?
+        """,
+    )
+
+    # Run through everything else
+    t_create_tmpdir = CreateTmpDirOperator(task_id="create_tmpdir")
+    # Copy results over to new dataset (extras/transformations)
 
     t_set_dataset_processing = PythonOperator(
         python_callable=pythonop_set_dataset_state,
         provide_context=True,
         op_kwargs={
-            "dataset_uuid_callable": get_parent_dataset_uuid,
+            "dataset_uuid_callable": epic_get_dataset_uuid_to_process,
         },
         task_id="set_dataset_processing",
     )
@@ -97,9 +158,10 @@ with HMDAG(
 
     def build_cwltool_cwl_tsv_to_mudata(**kwargs):
         run_id = kwargs["run_id"]
+        ti = kwargs["ti"]
         tmpdir = get_tmp_dir_path(run_id)
         print("tmpdir: ", tmpdir)
-        data_dir = get_parent_data_dir(**kwargs)
+        data_dir = ti.xcom_pull(task_ids="create_or_use_dataset")
         print("data_dir: ", data_dir)
 
         workflow = cwl_workflows["mudata"]
@@ -147,7 +209,8 @@ with HMDAG(
         task_id="move_data",
         bash_command="""
             tmp_dir="{{tmp_dir_path(run_id)}}" ; \
-            ds_dir="{{dag_run.conf['parent_lz_path']}}/extras/transformations" ; \
+            ds_dir="{{ti.xcom_pull(task_ids="create_or_use_dataset")}}/extras/transformations" ; \
+            mkdir "$ds_dir" ; \
             pushd "$ds_dir" ; \
             popd ; \
             mv "$tmp_dir"/cwl_out/* "$ds_dir" >> "$tmp_dir/session.log" 2>&1 ; \
@@ -164,7 +227,7 @@ with HMDAG(
             "move_data",
         ],
         cwl_workflows=list(cwl_workflows.values()),
-        dataset_uuid_fun=get_parent_dataset_uuid,
+        dataset_uuid_fun=epic_get_dataset_uuid_to_process,
     )
 
     t_send_status = PythonOperator(
@@ -173,11 +236,11 @@ with HMDAG(
 
     t_set_dataset_error = PythonOperator(
         task_id="set_dataset_error",
-        python_callable=utils.pythonop_set_dataset_state,
+        python_callable=pythonop_set_dataset_state,
         provide_context=True,
         trigger_rule="all_done",
         op_kwargs={
-            "dataset_uuid_callable": get_parent_dataset_uuid,
+            "dataset_uuid_callable": epic_get_dataset_uuid_to_process,
             "ds_state": "Error",
             "message": "An error occurred in {}".format(pipeline_name),
         },
@@ -188,6 +251,8 @@ with HMDAG(
 
     (
         t_log_info
+        >> t_create_or_use_dataset
+        >> t_copy_data
         >> t_create_tmpdir
         >> t_set_dataset_processing
         >> prepare_cwl_tsv_to_mudata
