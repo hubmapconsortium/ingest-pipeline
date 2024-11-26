0a83e3eb69d824b003aad327d5728618e4cc63ca
David Betancur <dbetancur@psc.edu>
2024-11-26 17:47:16 -0500

    Updating urls for test environment.

--- src/ingest-pipeline/misc/tools/airflow_environments/env_cmudev.sh
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_cmudev.sh
@@ -1,6 +1,6 @@
 #set airflow environment
-HM_AF_CONFIG=/hive/users/hive/hubmap/hivevm202-cmudev/ingest-pipeline/src/ingest-pipeline/airflow/airflow.cfg
-HM_AF_HOME=/hive/users/hive/hubmap/hivevm202-cmudev/ingest-pipeline/src/ingest-pipeline/airflow
+HM_AF_CONFIG=/hive/users/hive/hubmap/vm023-cmudev/ingest-pipeline/src/ingest-pipeline/airflow/airflow.cfg
+HM_AF_HOME=/hive/users/hive/hubmap/vm023-cmudev/ingest-pipeline/src/ingest-pipeline/airflow
 
 HM_AF_CONN_INGEST_API_CONNECTION=http:/vm002.psc.edu:7777/
 HM_AF_CONN_UUID_API_CONNECTION=http://https%3a%2f%2fuuid-api.test.hubmapconsortium.org/
