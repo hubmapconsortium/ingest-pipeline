a5ddbd4ff275dde2dccc907348eb951c1c792829
David Betancur <dbetancur@psc.edu>
2024-11-26 17:59:00 -0500

    TyPo on URL

--- src/ingest-pipeline/misc/tools/airflow_environments/env_cmudev.sh
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_cmudev.sh
@@ -2,7 +2,7 @@
 HM_AF_CONFIG=/hive/users/hive/hubmap/vm023-cmudev/ingest-pipeline/src/ingest-pipeline/airflow/airflow.cfg
 HM_AF_HOME=/hive/users/hive/hubmap/vm023-cmudev/ingest-pipeline/src/ingest-pipeline/airflow
 
-HM_AF_CONN_INGEST_API_CONNECTION=http:/vm002.hive.psc.edu:7777/
+HM_AF_CONN_INGEST_API_CONNECTION=http://vm002.hive.psc.edu:7777/
 HM_AF_CONN_UUID_API_CONNECTION=http://https%3a%2f%2fuuid-api.test.hubmapconsortium.org/
 HM_AF_CONN_FILES_API_CONNECTION=http://https%3a%2f%2ffiles-api.test.hubmapconsortium.org/
 HM_AF_CONN_SPATIAL_API_CONNECTION=http://https%3a%2f%2fspatial-api.test.hubmapconsortium.org/
