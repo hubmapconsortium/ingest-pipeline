ff7e4eaaabeac83b790b256cb8c59c74f20bf17e
Gesina Phillips <gesina@psc.edu>
2025-05-19 17:08:35 -0400

    slack messaging integration for priority upload reorganization

--- src/ingest-pipeline/airflow/dags/status_change/status_utils.py
+++ src/ingest-pipeline/airflow/dags/status_change/status_utils.py
@@ -74,7 +74,7 @@ ENTITY_STATUS_MAP = {
 
 
 # This is simplified from pythonop_get_dataset_state in utils
-def get_submission_context(token: str, uuid: str) -> Dict[str, Any]:
+def get_submission_context(token: str, uuid: str) -> dict[str, Any]:
     """
     uuid can also be a HuBMAP ID.
     """
