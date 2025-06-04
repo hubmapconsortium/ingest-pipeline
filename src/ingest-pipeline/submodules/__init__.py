dc9afe1c459a40e6131e1186ceb3ad4fc8134703
Gesina Phillips <gesina@psc.edu>
2025-06-04 15:54:38 -0400

    removing unused or deleted submodules

--- src/ingest-pipeline/submodules/__init__.py
+++ src/ingest-pipeline/submodules/__init__.py
@@ -4,7 +4,6 @@ from importlib import import_module
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "ingest-validation-tools", "src"))
 sys.path.append(os.path.join(os.path.dirname(__file__), "ingest-validation-tests", "src"))
-sys.path.append(os.path.join(os.path.dirname(__file__), "hubmap-inventory"))
 
 ingest_validation_tools_upload = import_module("ingest_validation_tools.upload")
 ingest_validation_tools_error_report = import_module("ingest_validation_tools.error_report")
@@ -15,7 +14,6 @@ ingest_validation_tools_plugin_validator = import_module(
     "ingest_validation_tools.plugin_validator"
 )
 ingest_validation_tests = import_module("ingest_validation_tests")
-hubmapinventory = import_module("hubmapinventory")
 
 __all__ = [
     "ingest_validation_tools_validation_utils",
@@ -23,9 +21,7 @@ __all__ = [
     "ingest_validation_tools_error_report",
     "ingest_validation_tools_plugin_validator",
     "ingest_validation_tests",
-    "hubmapinventory",
 ]
 
-sys.path.pop()
 sys.path.pop()
 sys.path.pop()
