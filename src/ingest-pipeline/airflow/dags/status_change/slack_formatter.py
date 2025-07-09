199c56e669cb4c1e0c3618c20a3e7d48c004404b
Gesina Phillips <gesina@psc.edu>
2025-07-09 16:41:53 -0400

    adding globus urls to reorganized priority project dataset output

--- src/ingest-pipeline/airflow/dags/status_change/slack_formatter.py
+++ src/ingest-pipeline/airflow/dags/status_change/slack_formatter.py
@@ -1,3 +1,4 @@
+import urllib.parse
 from typing import Optional
 
 from airflow.configuration import conf as airflow_conf
@@ -39,6 +40,35 @@ def format_priority_reorganized_msg(token: str, uuid: str) -> tuple[str, str]:
     return msg, priority_reorganized_channel
 
 
+def _get_abs_path(uuid: str, token: str, return_globus: bool = True) -> str:
+    """
+    Return either the Globus URL (default) or filesystem absolute path.
+    URL format is https://app.globus.org/file-manager?origin_id=<id>&origin_path=<uuid | consortium|private/<group>/<uuid>>
+    """
+    http_hook = HttpHook("GET", http_conn_id="ingest_api_connection")
+    headers = {
+        "authorization": f"Bearer {token}",
+        "content-type": "application/json",
+        "X-Hubmap-Application": "ingest-pipeline",
+    }
+    response = http_hook.run(
+        endpoint=f"datasets/{uuid}/file-system-abs-path",
+        headers=headers,
+    )
+    path = response.json().get("path")
+    if not return_globus:
+        return path
+    prefix = "https://app.globus.org/file-manager?"
+    params = {}
+    if "public" in path:
+        params["origin_id"] = "af603d86-eab9-4eec-bb1d-9d26556741bb"
+        params["origin_path"] = uuid
+    else:
+        params["origin_id"] = "24c2ee95-146d-4513-a1b3-ac0bfdb7856f"
+        params["origin_path"] = path.replace("/hive/hubmap/data", "") + "/"
+    return prefix + urllib.parse.urlencode(params)
+
+
 def _format_full_priority_reorganized_msg(
     upload_label: str, data: dict[str, str], dataset_info: list[str]
 ) -> str:
@@ -64,14 +94,17 @@ def _format_upload_reorganized_datasets(datasets: list[dict], token: str) -> lis
         "priority_project_list",
         "dataset_type",
         "organ",
+        "globus_url",
     ]
     info = [", ".join(keys)]
     for dataset in datasets:
-        # Organ is derived from additional API call, don't
-        # want it in the keys we look for in dataset
+        # Organ and globus_url are derived from additional API call, don't
+        # want them in the keys we look for in dataset
         data = [dataset.get(key, "") for key in keys if key != "organ"]
         organ = get_organ(dataset.get("uuid", ""), token)
         data.append(organ if organ else "")
+        globus_url = _get_abs_path(dataset.get("uuid", ""), token)
+        data.append(globus_url if globus_url else "")
         # Datasets should be formatted as lines of comma-separated strings
         # with internal ; delimiters.
         cleaned_data = []
