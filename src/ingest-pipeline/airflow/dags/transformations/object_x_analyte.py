31f3e30797af4b2b8e59898d7c6de1b4145edab7
Juan Puerto <=>
2025-08-21 12:44:49 -0400

    fix: syntax fixes

--- src/ingest-pipeline/airflow/dags/transformations/object_x_analyte.py
+++ src/ingest-pipeline/airflow/dags/transformations/object_x_analyte.py
@@ -158,8 +158,10 @@ with HMDAG(
         task_id="set_dataset_processing",
     )
 
-    # BEGIN - TSV -> MuData Region
-    prepare_cwl_tsv_to_mudata = EmptyOperator(task_id="prepare_cwl_tsv_to_mudata")
+    # BEGIN - Object by Analyte to UI Region
+    t_prepare_cwl_object_by_analyte_to_ui = EmptyOperator(
+        task_id="prepare_cwl_object_by_analyte_to_ui"
+    )
 
     def build_cwl_cmd_object_by_analyte_to_ui(**kwargs):
         run_id = kwargs["run_id"]
@@ -210,7 +212,7 @@ with HMDAG(
             "test_op": "pipeline_exec_cwl_object_by_analyte_to_ui",
         },
     )
-    # END - TSV -> MuData Region
+    # END - Object by Analyte to UI Region
 
     # Move data back to dataset_uuid
     t_move_data = BashOperator(
@@ -240,7 +242,9 @@ with HMDAG(
         data_dir = kwargs["ti"].xcom_pull(task_ids="create_or_use_dataset")
         try:
             output_metadata = json.load(
-                open(f"{data_dir}/extras/transformations/hubmap_ui/calculated_metadata.json")
+                open(
+                    f"{data_dir}/extras/transformations/hubmap_ui/mudata-zarr/calculated_metadata.json"
+                )
             )
         except FileNotFoundError as e:
             print(
@@ -291,6 +295,7 @@ with HMDAG(
         >> t_create_tmpdir
         >> t_copy_data
         >> t_set_dataset_processing
+        >> prepare_cwl_object_by_analyte_to_ui
         >> t_build_cwl_cmd_object_by_analyte_to_ui
         >> t_pipeline_exec_cwl_object_by_analyte_to_ui
         >> t_maybe_keep_cwl_object_by_analyte_to_ui
