876593064449bd1e0484ec87e55e6ff6f064a0b4
Joel Welling <welling@psc.edu>
2025-09-09 15:02:28 -0400

    Add --nmax option

--- src/ingest-pipeline/misc/tools/zarr_zipper.py
+++ src/ingest-pipeline/misc/tools/zarr_zipper.py
@@ -5,7 +5,6 @@ import re
 import os
 import json
 from pathlib import Path
-from pprint import pprint
 from typing import List, Tuple, TypeVar, Union
 import logging
 from subprocess import run
@@ -15,6 +14,17 @@ LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 UUID_REGEX = re.compile(r"[0123456789aAbBcCdDeEfF]{32}")
+
+
+class TargetProps:
+    def __init__(self, target_path: Path, zarr_name: str, is_dataset: bool, is_published: bool):
+        self.target_path = target_path
+        self.zarr_name = zarr_name
+        self.is_dataset = is_dataset
+        self.is_published = is_published
+    def __str__(self):
+        return f"TargetProps({self.target_path}, {self.zarr_name}, {self.is_dataset}, {self.is_published})"
+
 
 def parse_path(path) -> Tuple[Path, str, str]:
     LOGGER.debug(f"starting with {path}")
@@ -42,6 +52,7 @@ def run_zip(target_path: Path, zarr_name: str, delete_flg: bool) -> None:
     assert target_path != target_path.anchor, "zip is aimed at /"
     assert full_target_path != target_path.anchor, "zip is aimed at /"
     zip_name = target_path / f"{zarr_name}.zip"
+    zip_name = zip_name.absolute()
     cmd0 = "( [[ $PWD != '/' ]] && [[ $PWD != '/hive' ]] )"
     cmd1 = f"cd '{full_target_path}'"
     cmd2 = f"zip -r '{zip_name}' ."
@@ -57,23 +68,31 @@ def run_zip(target_path: Path, zarr_name: str, delete_flg: bool) -> None:
 def restructure(
         candidate: Path,
         dryrun: bool = False,
-        delete_flg: bool = False) -> str:
+        delete_flg: bool = False,
+        allow_published: bool = False) -> str:
     try:
         target_path, zarr_name, uuid = parse_path(candidate)
     except RuntimeError as excp:
         LOGGER.warning(f"error parsing {candidate}: {excp}")
         return None
-    if dryrun:
+    is_dataset, is_published = verify_uuid(uuid)
+    props = TargetProps(target_path, zarr_name, is_dataset, is_published)
+    LOGGER.info(f"{props}")
+    if props.is_published and not allow_published:
+        LOGGER.info(f"allow_published is False and {uuid} is published; not zipping {zarr_name}")
+    elif dryrun:
         LOGGER.info(f"dryrun is true; not zipping {zarr_name}")
     else:
         run_zip(target_path, zarr_name, delete_flg)
-    return uuid
+    return uuid, props
 
 
-def verify_uuid(uuid: str) -> bool:
+def verify_uuid(uuid: str) -> Tuple[bool, bool]:
     """
     Take ENTITY_API and AUTH_TOK from the environment and look up the given uuid.
-    Returns True if a valid result is found, False otherwise.
+    Returns a tuple (bool, bool) where:
+      the first value is True if a valid result is found, False otherwise.
+      the second value is True if the result exists and is Published, False otherwise
     """
     auth_tok = os.environ.get("AUTH_TOK")
     if not auth_tok:
@@ -91,8 +110,8 @@ def verify_uuid(uuid: str) -> bool:
         resp.raise_for_status()
     except Exception as excp:
         LOGGER.error(f"Validation of {uuid} failed: {type(excp)} {excp}")
-        return False
-    return True
+        return False, False
+    return True, resp.json()['status'].lower() == 'published'
 
 
 def main() -> None:
@@ -118,23 +137,46 @@ def main() -> None:
         help="actually rm the non-zipped directory after zipping",
         action="store_true",
     )
+    parser.add_argument(
+        "--allow_published",
+        help="published datasets are excluded from zipping if this flag is not set",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--nmax",
+        help="do no more than this many target directories (0 means no limit)",
+        type=int,
+        default=0
+    )
+        
 
     args = parser.parse_args()
 
     dryrun = args.dryrun
     delete_flg = args.delete
+    allow_published = args.allow_published
+    nmax = args.nmax
 
     start_path = Path(args.starting_path)
     if not start_path.is_dir():
         parser.error(f"{start_path} is not an existing directory")
 
     all_valid_uuids = []
-    for candidate in start_path.glob(args.proto):
+    for idx, candidate in enumerate(start_path.glob(args.proto)):
+        if nmax and idx >= nmax:
+            break
         LOGGER.info(f"testing {candidate}")
-        uuid = restructure(candidate, dryrun=dryrun, delete_flg=delete_flg)
-        if uuid and verify_uuid(uuid):
+        uuid, props = restructure(
+            candidate,
+            dryrun=dryrun,
+            delete_flg=delete_flg,
+            allow_published=allow_published
+        )
+        if uuid and props.is_dataset:
             all_valid_uuids.append(uuid)
 
+    all_valid_uuids = list(set(all_valid_uuids))
+    print(f"{len(all_valid_uuids)} uuids to update")
     print(f"rebuilding json: {json.dumps({'uuids':all_valid_uuids})}")
 
 if __name__ == "__main__":
