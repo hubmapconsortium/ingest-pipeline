cfc2cc29c418383f9c05c0cee691e76d383142a9
Juan Puerto <=>
2025-09-16 14:55:56 -0400

    fix: Test travis removal

--- test.sh
+++ test.sh
@@ -13,8 +13,8 @@ red="$(tput setaf 1)"
 green="$(tput setaf 2)"
 reset="$(tput sgr0)"
 
-start() { [[ -z "$CI" ]] || echo "travis_fold:start:$1"; echo "$green$1$reset"; }
-end() { [[ -z "$CI" ]] || echo "travis_fold:end:$1"; }
+start() { echo "$green$1$reset"; }
+end() { :; }
 die() { set +v; echo "$red$*$reset" 1>&2 ; exit 1; }
 
 start placeholder
