17d975bf6f7a067f074143539fa40922a55c3de3
Juan Puerto <=>
2025-09-25 14:40:15 -0400

    fix: fix auth tok

--- src/ingest-pipeline/airflow/dags/undo_upload_reorganization.py
+++ src/ingest-pipeline/airflow/dags/undo_upload_reorganization.py
@@ -92,7 +92,7 @@ with HMDAG(
 
     def instantiate_factories():
         # Need an EntityFactory
-        auth_tok = encrypt_tok(airflow_conf.as_dict()["connections"]["APP_CLIENT_SECRET"]).decode()
+        auth_tok = airflow_conf.as_dict()["connections"]["APP_CLIENT_SECRET"]
         return EntityFactory(
             auth_tok,
             instance=find_matching_endpoint(HttpHook.get_connection("entity_api_connection").host),
