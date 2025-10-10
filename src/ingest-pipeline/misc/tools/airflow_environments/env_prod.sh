fc127503015fd47c30b250803210ddf4b6f97ad5
Juan Puerto <=>
2025-10-10 15:36:58 -0400

    general: remove spatial api references

--- src/ingest-pipeline/misc/tools/airflow_environments/env_prod.sh
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_prod.sh
@@ -5,7 +5,6 @@ HM_AF_HOME=/opt/repositories/vm004-prod/ingest-pipeline/src/ingest-pipeline/airf
 HM_AF_CONN_INGEST_API_CONNECTION=http://vm004.hive.psc.edu:7777/
 HM_AF_CONN_UUID_API_CONNECTION=http://https%3a%2f%2fuuid.api.hubmapconsortium.org/
 HM_AF_CONN_FILES_API_CONNECTION=http://https%3a%2f%2ffiles.api.hubmapconsortium.org/
-HM_AF_CONN_SPATIAL_API_CONNECTION=http://https%3a%2f%2fspatial.api.hubmapconsortium.org/
 HM_AF_CONN_CELLS_API_CONNECTION=http://https%3a%2f%2fcells.api.hubmapconsortium.org/
 HM_AF_CONN_SEARCH_API_CONNECTION=http://https%3a%2f%2fsearch.api.hubmapconsortium.org/v3/
 HM_AF_CONN_ENTITY_API_CONNECTION=http://https%3a%2f%2fentity.api.hubmapconsortium.org/
