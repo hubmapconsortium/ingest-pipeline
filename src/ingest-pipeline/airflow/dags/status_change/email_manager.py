a7b04b29cc1ee1044241c6c77670b922d8fcabd0
Gesina Phillips <gesina@psc.edu>
2025-10-15 17:14:24 -0400

    fixes from testing

--- src/ingest-pipeline/airflow/dags/status_change/email_manager.py
+++ src/ingest-pipeline/airflow/dags/status_change/email_manager.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 from airflow.utils.email import send_email
 
@@ -13,7 +14,7 @@ from .status_utils import (
 
 
 class EmailManager:
-    int_recipients = []  # TODO
+    int_recipients = []  # TODO: spec
     main_recipients = ""
     cc = ""
     subj = ""
@@ -65,7 +66,7 @@ class EmailManager:
             "qa",
             "reorganized",
             "valid",
-        ]:  # TODO: determine statuses requiring ext emails
+        ]:  # TODO: spec (determine statuses requiring ext emails)
             subj, msg = self.generic_good_status_format()
         elif self.status == "invalid":  # actually invalid
             subj, msg = self.get_ext_invalid_format()
@@ -103,7 +104,7 @@ class EmailManager:
         Primary contact: {self.primary_contact}<br>
         Ingest page: {get_entity_ingest_url(self.entity_data)}<br>
         Log file: {log_directory_path(self.run_id)}<br>
-        </br>
+        <br></br>
         Error:<br>
             {self._parsed_error()}
         """
@@ -111,28 +112,27 @@ class EmailManager:
 
     def get_ext_invalid_format(self) -> tuple[str, str]:
         subj = f"{self.entity_type} {self.entity_id} is invalid"
-        # TODO: ask about any instructions required here
+        # TODO: spec (ask about any instructions required here)
         msg = f"""
         {self.project.value[1]} ID: {self.entity_id}<br>
         Group: {self.entity_data.get('group_name')}<br>
         Ingest page: {get_entity_ingest_url(self.entity_data)}<br>
-        </br>
+        <br></br>
         {self.entity_type} is invalid:<br>
             {self._parsed_error()}
         """
         return subj, msg
 
     def get_recipients(self):
-        self.main_recipients = ", ".join(self.int_recipients)
-        self.cc = ", ".join(self.int_recipients)
-        # TODO: turning off any ext emails for testing
-        # if self.is_internal_error:
-        #     self.main_recipients = ", ".join(self.int_recipients)
-        # else:
-        #     self.main_recipients = self.primary_contact
-        #     self.cc = ", ".join(self.int_recipients)
+        if self.is_internal_error:
+            self.main_recipients = ", ".join(self.int_recipients)
+        else:
+            # TODO: turning off any ext emails for testing
+            # self.main_recipients = self.primary_contact
+            self.main_recipients = ", ".join(self.int_recipients)
+            self.cc = ", ".join(self.int_recipients)
 
     def _parsed_error(self):
-        return "<br>     ".join(
-            [line for line in self.entity_data.get("error_message", "").split("; ")]
+        return "<br>- ".join(
+            [line for line in re.split("; | \\| ", self.entity_data.get("error_message", ""))]
         )
