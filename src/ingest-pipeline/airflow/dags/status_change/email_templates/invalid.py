fd378275558923d71f3c51f7e0cb5bc8d3f51805
Gesina Phillips <gesina@psc.edu>
2025-11-13 14:27:32 -0500

    slight reword to reorg

--- src/ingest-pipeline/airflow/dags/status_change/email_templates/invalid.py
+++ src/ingest-pipeline/airflow/dags/status_change/email_templates/invalid.py
@@ -14,15 +14,13 @@ class InvalidStatusEmail(EmailTemplate):
             "The validation process starts by checking metadata TSVs and directory structures. If those checks pass, then certain individual file types (such as FASTQ and OME.TIFF files) are validated.",
             "",
             "<b>What to do next</b>",
-            f"If you have questions about your {self.data.entity_type.lower()}, please schedule an appointment with Data Curator Brendan Honick: https://calendly.com/bhonick-psc/.",
-            "",
-            "This email address is not monitored. If you have questions, please schedule with Brendan Honick or email ingest@hubmapconsortium.org.",
+            f"If you have questions about your {self.data.entity_type.lower()}, please schedule an appointment with Data Curator Brendan Honick (https://calendly.com/bhonick-psc/) or email ingest@hubmapconsortium.org. Do not respond to this email; this inbox is not monitored.",
         ]
         if self.data.error_dict:
             msg.extend(
                 [
                     "",
-                    "The error log is included below if you would like to make updates to your submission independently; it is not required for you to do so before contacting our Data Curation Team. Please email ingest@hubmapconsortium.org if you believe you have repaired all validation errors so that we can re-validate your submission.",
+                    "The error log is included below if you would like to make updates to your submission independently. It is not required for you to correct any errors before contacting our Data Curation Team. Please email ingest@hubmapconsortium.org if you believe you have repaired all validation errors so that we can re-validate your submission.",
                     "",
                     'If your submission has "Spreadsheet Validator Errors," please use the <a href="https://metadatavalidator.metadatacenter.org/">Metadata Spreadsheet Validator</a> tool to correct them.',
                     "",
