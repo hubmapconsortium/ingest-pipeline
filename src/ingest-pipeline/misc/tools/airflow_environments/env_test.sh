60d43c444f72787ab11ef724f2b8680edec978e6
David Betancur <dbetancur@psc.edu>
2025-11-19 13:36:37 -0500

    Bugfix missing search_api connection for TEST environment

--- src/ingest-pipeline/misc/tools/airflow_environments/env_test.sh
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_test.sh
@@ -6,5 +6,5 @@ HM_AF_CONN_INGEST_API_CONNECTION=http://vm002.hive.psc.edu:7777/
 HM_AF_CONN_UUID_API_CONNECTION=http://https%3a%2f%2fuuid-api.test.hubmapconsortium.org/
 HM_AF_CONN_FILES_API_CONNECTION=http://https%3a%2f%2ffiles-api.test.hubmapconsortium.org/
 HM_AF_CONN_CELLS_API_CONNECTION=http://https%3a%2f%2fcells-api.test.hubmapconsortium.org/
-HM_AF_CONN_SEARCH_API_CONNECTION=http://https%3a%2f%2fontology-api.test.hubmapconsortium.org/
+HM_AF_CONN_SEARCH_API_CONNECTION=http://https%3a%2f%2fsearch-api.test.hubmapconsortium.org/v3/
 HM_AF_CONN_ENTITY_API_CONNECTION=http://https%3a%2f%2fentity-api.test.hubmapconsortium.org/
