09232879d3c4495fc4541825386d249883bcc392
Juan Puerto <=>
2025-12-09 11:30:19 -0500

    fix: create a run_id directory for each h5ad file and copy that file into there (for now).

--- src/ingest-pipeline/airflow/dags/phenocycler_deepcell.py
+++ src/ingest-pipeline/airflow/dags/phenocycler_deepcell.py
@@ -190,8 +190,9 @@ with HMDAG(
     t_copy_stellar_pre_convert_data = BashOperator(
         task_id="copy_stellar_pre_convert_data",
         bash_command=""" \
+        mkdir /hive/hubmap/projects/STELLAR_pre_convert/{{run_id}}/
         tmp_dir={{tmp_dir_path(run_id)}} ; \
-        find ${tmp_dir} -name "cell_data.h5ad" -exec cp -v {} /hive/hubmap/projects/ \; ; \
+        find ${tmp_dir} -name "cell_data.h5ad" -exec cp -v {} /hive/hubmap/projects/STELLAR_pre_convert/{{run_id}} \; ; \
         echo $?
         """,
     )
