dc3ea163f65f85ceddda8299c1a321d25faddda1
David Betancur <dbetancur@psc.edu>
2025-12-11 16:36:08 -0500

    Bugfix dataframe header

--- src/ingest-pipeline/airflow/dags/status_change/statistics_manager.py
+++ src/ingest-pipeline/airflow/dags/status_change/statistics_manager.py
@@ -45,7 +45,7 @@ class StatisticsManager(MessageManager):
             df = pd.DataFrame({"uuid": self.uuid,
                                "dataset_type": self.dataset_type,
                                "directory": self.path}, index=[0])
-            statistics_path = log_directory_path(self.run_id)+ "datasets.csv"
+            statistics_path = log_directory_path(self.run_id) + "datasets.csv"
             df.to_csv(Path(statistics_path), index=False)
             df = calculate_statistics(statistics_path)
             df.to_csv(Path(utils.get_statistics_base_path() / "dataset_usage.csv"), mode="a",
