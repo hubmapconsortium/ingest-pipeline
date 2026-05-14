18177542990dbf0970af2a5d14395f36796f10d2
Joel Welling <welling@psc.edu>
2026-05-14 19:45:10 -0400

    add test case for $

--- tests/test_files_templater.py
+++ tests/test_files_templater.py
@@ -12,4 +12,23 @@ class TestFilesTemplater(unittest.TestCase):
         templated_files_l = TemplateBuilder(files_l).apply()
         expanded_files_l = TemplateBuilder.expand(templated_files_l)
         self.assertEqual(files_l, expanded_files_l)
-    
+
+    def test_dollar_case(self):
+        from template_utils import TemplateBuilder
+        files_l = [
+            {"rel_path": "ometiff-pyramids/pipeline_output/mask/reg001_mask.ome.tif",
+             "type": "unknown", "size": 9011601515,
+             "description": "OME-TIFF$ $ pyrami$d file",
+             "edam_term": "EDAM_1.24.format_3727",
+             "is_qa_qc": False, "is_data_product": False
+             },
+            {"rel_path": "ometiff-pyramids/pipeline_output/expr/reg001_expr.ome.tif",
+             "type": "unknown", "size": 30416407302,
+             "description": "OME-TIFF pyramid file", 
+             "edam_term": "EDAM_1.24.format_3727",
+             "is_qa_qc": False, "is_data_product": False
+             }
+        ]
+        templated_files_l = TemplateBuilder(files_l).apply()
+        expanded_files_l = TemplateBuilder.expand(templated_files_l)
+        self.assertEqual(files_l, expanded_files_l)
